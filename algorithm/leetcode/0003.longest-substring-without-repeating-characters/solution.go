d4e24d1243736794ed87673a8fe32a466fe1a9b3
stayhungry <lewis_sun@foxmail.com>
2021-09-29 09:29:28 +0800

    add solution for leecode 3

--- algorithm/leetcode/0003.longest-substring-without-repeating-characters/solution.go
+++ algorithm/leetcode/0003.longest-substring-without-repeating-characters/solution.go
@@ -7,3 +7,37 @@
  */
 
 package _003_longest_substring_without_repeating_characters
+
+// 核心思想: 滑动窗口
+func lengthOfLongestSubstring(s string) int {
+	window := make(map[uint8]int)
+	left, right := 0, 0
+	res := 0
+	length := len(s)
+	for right < length {
+		c := s[right]
+		right++
+
+		// 进行窗口内数据的一系列更新
+		window[c]++
+		// 判断左侧窗口是否需要收缩
+		for window[c] > 1 {
+			// 有重复
+			d := s[left]
+			left++
+
+			// 进行窗口内数据的一系列更新
+			window[d]--
+		}
+		// 在这里更新答案
+		res = max(res, right-left)
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- algorithm/leetcode/0003.longest-substring-without-repeating-characters/solution_test.go
+++ algorithm/leetcode/0003.longest-substring-without-repeating-characters/solution_test.go
@@ -7,3 +7,46 @@
  */
 
 package _003_longest_substring_without_repeating_characters
+
+import "testing"
+
+type para struct {
+	one string
+}
+
+type ans struct {
+	one int
+}
+
+type question struct {
+	para
+	ans
+}
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	qs := []question{
+		{
+			para{one: "abcabcbb"},
+			ans{one: 3},
+		},
+		{
+			para{one: "bbbbb"},
+			ans{one: 1},
+		},
+		{
+			para{one: "pwwkew"},
+			ans{one: 3},
+		},
+		{
+			para{one: ""},
+			ans{one: 0},
+		},
+	}
+
+	for _, q := range qs {
+		output := lengthOfLongestSubstring(q.para.one)
+		if output != q.ans.one {
+			t.Errorf("para:%v, ans:%v, output:%v", q.para.one, q.ans.one, output)
+		}
+	}
+}
