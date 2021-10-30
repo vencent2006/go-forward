2da5ad0fa06712bdc2ac6005099b1ef83656e30e
stayhungry <lewis_sun@foxmail.com>
2021-10-30 20:29:01 +0800

    add handwriting-web-inf geek14(based on geek13)

---
+++ course/handwriting-web-inf/code_14/framework/util/exec.go
@@ -0,0 +1,38 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  exec
+ * @Version: 1.0.0
+ * @Date: 2021/10/29 13:39
+ */
+
+package util
+
+import (
+	"os"
+	"syscall"
+)
+
+// GetExecDirectory 获取当前执行程序目录
+func GetExecDirectory() string {
+	file, err := os.Getwd()
+	if err == nil {
+		return file + "/"
+	}
+	return ""
+}
+
+// CheckProcessExist will return true if the process with PID exists
+func CheckProcessExist(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+
+	err = process.Signal(syscall.Signal(0))
+	if err != nil {
+		return false
+	}
+
+	return true
+}
