29dbcd5ba530e8061149bbd7a665e5117df38fef
stayhungry <lewis_sun@foxmail.com>
2021-11-02 17:07:08 +0800

    add handwriting-web-inf geek17(based on geek16)

---
+++ course/handwriting-web-inf/code_17/framework/command/kernel.go
@@ -0,0 +1,19 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  kernel
+ * @Version: 1.0.0
+ * @Date: 2021/10/29 10:07
+ */
+
+package command
+
+import "go-examples/course/handwriting-web-inf/code_17/framework/cobra"
+
+// AddKernelCommands will add all command/* to root command
+func AddKernelCommands(root *cobra.Command) {
+	root.AddCommand(DemoCommand)       // demo 命令
+	root.AddCommand(initAppCommand())  // app 命令
+	root.AddCommand(initEnvCommand())  // env 命令
+	root.AddCommand(initCronCommand()) // cron 命令
+}
---
+++ course/handwriting-web-inf/code_17/framework/middleware/timeout.go
@@ -0,0 +1,50 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  timeout
+ * @Version: 1.0.0
+ * @Date: 2021/9/21 23:28
+ */
+
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"go-examples/course/handwriting-web-inf/code_17/framework/gin"
+	"log"
+	"time"
+)
+
+func Timeout(d time.Duration) gin.HandlerFunc {
+	// 使用函数回调
+	return func(c *gin.Context) {
+		finish := make(chan struct{}, 1)
+		panicChan := make(chan interface{}, 1)
+		// 执行业务逻辑前预操作：初始化超时context
+		durationCtx, cancel := context.WithTimeout(c.BaseContext(), d)
+		defer cancel()
+
+		go func() {
+			defer func() {
+				if p := recover(); p != nil {
+					panicChan <- p
+				}
+			}()
+			// 使用next执行具体的业务逻辑
+			c.Next()
+
+			finish <- struct{}{}
+		}()
+		// 执行业务逻辑后操作
+		select {
+		case p := <-panicChan:
+			c.ISetStatus(500).IJson("panic")
+			log.Println(p)
+		case <-finish:
+			fmt.Println("finish")
+		case <-durationCtx.Done():
+			c.ISetStatus(500).IJson("time out")
+		}
+	}
+}
