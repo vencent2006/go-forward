e85cfcb8e6a75c2f7fb368d76b46d4fc83729d14
stayhungry <lewis_sun@foxmail.com>
2021-11-03 19:07:16 +0800

    add handwriting-web-inf geek19(based on geek18)

---
+++ course/handwriting-web-inf/code_19/framework/provider/config/provider.go
@@ -0,0 +1,51 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  provider
+ * @Version: 1.0.0
+ * @Date: 2021/11/2 09:50
+ */
+
+package config
+
+import (
+	"go-examples/course/handwriting-web-inf/code_19/framework"
+	"go-examples/course/handwriting-web-inf/code_19/framework/contract"
+	"path/filepath"
+)
+
+type HadeConfigProvider struct {
+}
+
+// Register register a new function for make a service instance
+func (provider *HadeConfigProvider) Register(c framework.Container) framework.NewInstance {
+	return NewHadeConfig
+}
+
+// Boot will called when the service instantiate
+func (provider *HadeConfigProvider) Boot(c framework.Container) error {
+	return nil
+}
+
+// IsDefer define whether the service instantiate when first make or register
+func (provider *HadeConfigProvider) IsDefer() bool {
+	return false
+}
+
+// Params define the necessary params for NewInstance
+func (provider *HadeConfigProvider) Params(c framework.Container) []interface{} {
+	appService := c.MustMake(contract.AppKey).(contract.App)
+	envService := c.MustMake(contract.EnvKey).(contract.Env)
+	env := envService.AppEnv()
+
+	// 配置文件夹地址
+	configFolder := appService.ConfigFolder()
+	envFolder := filepath.Join(configFolder, env)
+
+	return []interface{}{c, envFolder, envService.All()}
+}
+
+// Name define the name for this service
+func (provider *HadeConfigProvider) Name() string {
+	return contract.ConfigKey
+}
---
+++ course/handwriting-web-inf/code_19/framework/provider/log/services/custom.go
@@ -0,0 +1,37 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  custom
+ * @Version: 1.0.0
+ * @Date: 2021/11/2 17:51
+ */
+
+package services
+
+import (
+	"go-examples/course/handwriting-web-inf/code_19/framework"
+	"go-examples/course/handwriting-web-inf/code_19/framework/contract"
+	"io"
+)
+
+type HadeCustomLog struct {
+	HadeLog
+}
+
+func NewHadeCustomLog(params ...interface{}) (interface{}, error) {
+	c := params[0].(framework.Container)
+	level := params[1].(contract.LogLevel)
+	ctxFielder := params[2].(contract.CtxFielder)
+	formatter := params[3].(contract.Formatter)
+	output := params[4].(io.Writer)
+
+	log := &HadeConsoleLog{}
+
+	log.SetLevel(level)
+	log.SetCtxFielder(ctxFielder)
+	log.SetFormatter(formatter)
+
+	log.SetOutput(output)
+	log.c = c
+	return log, nil
+}
