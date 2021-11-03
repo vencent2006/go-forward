9dd82847bb1ce59cfbedf90c2b9cd134be2046f7
stayhungry <lewis_sun@foxmail.com>
2021-11-03 09:15:07 +0800

    add algorithm 300.longest-increasing-subsequence

---
+++ algorithm/leetcode/0300.Longest-Increasing-Subsequence/solution.go
@@ -0,0 +1,32 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  solution
+ * @Version: 1.0.0
+ * @Date: 2021/11/3 00:42
+ */
+
+package _300_Longest_Increasing_Subsequence
+
+func lengthOfLIS(nums []int) int {
+	// dp[i] 表示以nums[i]结尾的最长递增子序列的长度
+	dp, res := make([]int, len(nums)), 0
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
---
+++ algorithm/leetcode/0300.Longest-Increasing-Subsequence/solution_test.go
@@ -0,0 +1,48 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  solution_test
+ * @Version: 1.0.0
+ * @Date: 2021/11/3 00:51
+ */
+
+package _300_Longest_Increasing_Subsequence
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question300 struct {
+	para300
+	ans300
+}
+
+// para 是参数
+// one 代表第一个参数
+type para300 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans300 struct {
+	one int
+}
+
+func Test300(t *testing.T) {
+	qs := []question300{
+		{
+			para300{[]int{10, 9, 2, 5, 3, 7, 101, 18}},
+			ans300{4},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 300------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans300, q.para300
+		fmt.Printf("【input】:%v       【output】:%v\n", p, lengthOfLIS(p.one))
+	}
+	fmt.Printf("\n\n\n")
+}
