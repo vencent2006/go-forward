5e7383e85ac5d4bd4cb8ce89ea1a415fdabbe370
stayhungry <lewis_sun@foxmail.com>
2021-11-16 13:54:58 +0800

    add handwriting-web-inf geek25(based on geek24)

---
+++ course/handwriting-web-inf/code_25/app/http/route.go
@@ -0,0 +1,36 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  router
+ * @Version: 1.0.0
+ * @Date: 2021/10/29 15:39
+ */
+
+package http
+
+import (
+	"go-examples/course/handwriting-web-inf/code_25/app/http/module/demo"
+	"go-examples/course/handwriting-web-inf/code_25/framework/contract"
+	"go-examples/course/handwriting-web-inf/code_25/framework/gin"
+	ginSwagger "go-examples/course/handwriting-web-inf/code_25/framework/middleware/gin-swagger"
+	"go-examples/course/handwriting-web-inf/code_25/framework/middleware/gin-swagger/swaggerFiles"
+	"go-examples/course/handwriting-web-inf/code_25/framework/middleware/static"
+	"log"
+)
+
+func Routes(r *gin.Engine) {
+	container := r.GetContainer()
+	configService := container.MustMake(contract.ConfigKey).(contract.Config)
+
+	// 根路径"/"先去"/frontend/dist"目录下，查找文件是否存在，找到使用文件服务提供服务
+	r.Use(static.Serve("/", static.LocalFile("./frontend/dist", false)))
+
+	// 如果配置了swagger, 则显示swagger的中间件
+	log.Println("app.swagger is ", configService.GetBool("app.swagger"))
+	if configService.GetBool("app.swagger") == true {
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
+
+	// 动态路由定义
+	demo.Register(r)
+}
---
+++ course/handwriting-web-inf/code_25/framework/provider/log/services/console.go
@@ -0,0 +1,38 @@
+/**
+ * @Author: vincent
+ * @Description:
+ * @File:  console
+ * @Version: 1.0.0
+ * @Date: 2021/11/2 17:50
+ */
+
+package services
+
+import (
+	"go-examples/course/handwriting-web-inf/code_25/framework"
+	"go-examples/course/handwriting-web-inf/code_25/framework/contract"
+	"os"
+)
+
+type HadeConsoleLog struct {
+	HadeLog
+}
+
+func NewHadeConsoleLog(params ...interface{}) (interface{}, error) {
+	container := params[0].(framework.Container)
+	level := params[1].(contract.LogLevel)
+	ctxFielder := params[2].(contract.CtxFielder)
+	formatter := params[3].(contract.Formatter)
+
+	log := &HadeConsoleLog{}
+
+	log.SetLevel(level)
+	log.SetCtxFielder(ctxFielder)
+	log.SetFormatter(formatter)
+
+	// 最重要的将内容输出到控制台
+	log.SetOutput(os.Stdout)
+	log.c = container
+
+	return log, nil
+}
