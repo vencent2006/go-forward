25b1f1eb08ad8e3096e89a0b8062afc3ccebf07f
stayhungry <lewis_sun@foxmail.com>
2021-12-15 10:19:15 +0800

    frontend:go_gateway_view_demo delete unused files

--- frontend/go_gateway_view_demo/src/views/dashboard/index.vue
+++ frontend/go_gateway_view_demo/src/views/dashboard/index.vue
@@ -1,31 +1,12 @@
 <template>
-  <div class="dashboard-container">
-    <component :is="currentRole" />
-  </div>
+  <div class="dashboard-container" />
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
-
 export default {
-  name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
+  name: "Dashboard",
   data() {
-    return {
-      currentRole: 'adminDashboard'
-    }
+    return {};
   },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
-  },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
-    }
-  }
-}
+};
 </script>
