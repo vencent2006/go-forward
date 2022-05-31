ba4f4720b77db0d340249e740ab83ea0cccb3c87
stayhungry <lewis_sun@foxmail.com>
2022-05-31 22:49:37 +0800

    gateway: 5-10 登录管理功能 - 退出接口开发

--- course/gateway/go_gateway_demo/controller/admin_login.go
+++ course/gateway/go_gateway_demo/controller/admin_login.go
@@ -28,6 +28,7 @@ type AdminLoginController struct{}
 func AdminLoginRegister(group *gin.RouterGroup) {
 	adminLogin := &AdminLoginController{}
 	group.POST("/login", adminLogin.AdminLogin)
+	group.GET("/logout", adminLogin.AdminLogout)
 }
 
 // AdminLogin godoc
@@ -81,4 +82,22 @@ func (adminLogin *AdminLoginController) AdminLogin(c *gin.Context) {
 	out := &dto.AdminLoginOutput{Token: admin.UserName}
 
 	middleware.ResponseSuccess(c, out)
+}
+
+//AdminLogout godoc
+//@Summary 管理员退出
+//@Description 管理员退出
+//@Tags 管理员接口
+//@ID /admin_login/logout
+//@Accept json
+//@Produce json
+//@Success 200 {object} middleware.Response{data=string} "success"
+//@Router /admin_login/logout [get]
+func (adminLogin *AdminLoginController) AdminLogout(c *gin.Context) {
+
+	sess := sessions.Default(c)
+	sess.Delete(public.AdminSessionInfoKey)
+	sess.Save()
+
+	middleware.ResponseSuccess(c, "")
 }
