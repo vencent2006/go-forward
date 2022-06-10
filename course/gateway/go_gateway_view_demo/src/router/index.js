d25193baa045572964dee022c9bd8ca752082b4e
stayhungry <lewis_sun@foxmail.com>
2022-06-10 17:01:09 +0800

    gateway: 6-21 租户的列表、增加、修改、统计

--- course/gateway/go_gateway_view_demo/src/router/index.js
+++ course/gateway/go_gateway_view_demo/src/router/index.js
@@ -116,6 +116,56 @@ export const constantRoutes = [
         hidden: true
       }
     ]
+  },
+  {
+    path: '/app',
+    redirect: '/app/app_list',
+    component: Layout,
+    name: '租户管理',
+    meta: {
+      title: '租户管理',
+      icon: 'user'
+    },
+    children: [{
+      path: 'app_list',
+      component: () => import('@/views/app/app_list'),
+      name: '租户列表',
+      meta: {
+        title: '租户列表',
+        icon: 'user',
+        affix: false
+      }
+    },
+    {
+      path: 'app_create',
+      component: () => import('@/views/app/app'),
+      name: 'CreateApp',
+      meta: {
+        title: '创建租户',
+        icon: 'edit'
+      },
+      hidden: true
+    },
+    {
+      path: 'app_edit/:id(\\d+)',
+      component: () => import('@/views/app/app'),
+      name: 'EditApp',
+      meta: {
+        title: '修改租户'
+      },
+      hidden: true
+    },
+    {
+      path: 'app_stat/:id(\\d+)',
+      component: () => import('@/views/app/stat'),
+      name: 'AppStat',
+      meta: {
+        title: '租户流量统计',
+        noCache: true
+      },
+      hidden: true
+    }
+    ]
   }
 ]
 
