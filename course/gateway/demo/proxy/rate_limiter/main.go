d91b5cef260dbec95196b084b864d3d4cbbc827b
stayhungry <lewis_sun@foxmail.com>
2022-06-11 19:24:48 +0800

    gateway: 7-5 给代理插上限流的翅膀

---
+++ course/gateway/demo/proxy/rate_limiter/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"go-examples/course/gateway/proxy/middleware"
+	"go-examples/course/gateway/proxy/proxy"
+)
+
+var addr = "127.0.0.1:2002"
+
+// 熔断方案
+func main() {
+	coreFunc := func(c *middleware.SliceRouterContext) http.Handler {
+		rs1 := "http://127.0.0.1:2003/base"
+		url1, err1 := url.Parse(rs1)
+		if err1 != nil {
+			log.Println(err1)
+		}
+
+		rs2 := "http://127.0.0.1:2004/base"
+		url2, err2 := url.Parse(rs2)
+		if err2 != nil {
+			log.Println(err2)
+		}
+
+		urls := []*url.URL{url1, url2}
+		return proxy.NewMultipleHostsReverseProxy(c, urls)
+	}
+	log.Println("Starting httpserver at " + addr)
+
+	sliceRouter := middleware.NewSliceRouter()
+	sliceRouter.Group("/").Use(middleware.RateLimiter())
+	routerHandler := middleware.NewSliceRouterHandler(coreFunc, sliceRouter)
+	log.Fatal(http.ListenAndServe(addr, routerHandler))
+}
