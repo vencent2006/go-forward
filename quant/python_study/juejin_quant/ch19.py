d27e9b57fcb1c40a694b23c363e9c1b210eaf9e0
stayhungry <lewis_sun@foxmail.com>
2022-09-06 19:40:32 +0800

    juejin_quant: ch19.py

--- quant/python_study/juejin_quant/ch19.py
+++ quant/python_study/juejin_quant/ch19.py
@@ -1,8 +1,11 @@
 import pandas as pd
-import pandas_datareader as web
+import pandas_datareader.data as web
 import datetime
 
-# 获取上证指数交易数据 pandas-datareade模块data.DataReader()方法
-df = web.DataReader("000001.SS", "yahoo", datetime.datetime(2018, 1, 1), datetime.datetime(2018, 2, 1))
+# 获取上证指数交易数据 pandas-datareader模块data.DataReader()方法
+df = web.DataReader("000001.SS", "yahoo", datetime.datetime(2017, 1, 1), datetime.datetime(2018, 2, 1))
+df = pd.DataFrame(df)
+print('#'*20)
+print(df.head(10))  # 查看前几行
+print('*'*20)
 
-df.head(10)  # 查看前几行
