116f54a358009ad5c447c0c2278638712b176fc3
stayhungry <lewis_sun@foxmail.com>
2022-11-26 20:03:21 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch2-4

--- java_proj/vincent-springboot-starter/src/main/java/com/vincent/controller/StuController.java
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/controller/StuController.java
@@ -3,17 +3,39 @@ package com.vincent.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 @RestController
 @RequestMapping("stu")
 @Slf4j
 public class StuController {
-    @GetMapping("get")
-    public Object get(){
+    @GetMapping("{stuId}/get")
+    public Object get(@PathVariable("stuId") String stuId,
+                      @RequestParam Integer id,
+                      @RequestParam String name){
+        /**
+         * @RequestParam 用于获取url中的请求参数，如果参数变量名保持一致，该注解可以省略
+         */
+        log.info("stuId = " + stuId);
+        log.info("id = " + id);
+        log.info("name = " + name);
+
         return "查询";
     }
 
     @PostMapping("post")
-    public Object post(){
+    public Object post(@RequestBody Map<String, Object> map,
+                       @RequestHeader("token") String token,
+                       @CookieValue("clientId") String clientId,
+                       HttpServletRequest request){
+        log.info("map = " + map.toString());
+        log.info("token = " + token);
+        log.info("clientId = " + clientId);
+
+        String tokenFromRequest = request.getHeader("token");
+        log.info("tokenFromRequest = " + tokenFromRequest);
+
         return "修改";
     }
 
