b38274098a09f1d9c7a0dcecbc154ce26cd58e6c
stayhungry <lewis_sun@foxmail.com>
2022-11-28 00:23:13 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch3-3 SpringBoot整合Mybatis的配置

--- java_proj/vincent-springboot-starter/src/main/java/com/vincent/my/mapper/MyMapper.java
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/my/mapper/MyMapper.java
@@ -1,4 +1,11 @@
 package com.vincent.my.mapper;
 
-public class MyMapper {
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 继承自己的MyMapper
+ * @param <T>
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
 }
