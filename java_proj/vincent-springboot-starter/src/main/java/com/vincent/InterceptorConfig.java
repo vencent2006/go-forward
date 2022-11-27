4491e683d57b327c7bb05cc709e5fcc582060cdb
stayhungry <lewis_sun@foxmail.com>
2022-11-27 15:43:40 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch2-9 SpringBoot 实现拦截器

---
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/InterceptorConfig.java
@@ -0,0 +1,22 @@
+package com.vincent;
+
+import com.vincent.controller.interceptor.UserInfoInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Bean
+    public UserInfoInterceptor userInfoInterceptor(){
+        return new UserInfoInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器
+        registry.addInterceptor(userInfoInterceptor())
+                .addPathPatterns("/upload");
+    }
+}
