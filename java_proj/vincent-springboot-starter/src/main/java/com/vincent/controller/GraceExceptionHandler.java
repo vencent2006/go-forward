5e7faef0a9f04a03be9aec4189081fd0b7327809
stayhungry <lewis_sun@foxmail.com>
2022-11-27 15:07:38 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch2-8 SpringBoot 之统一异常封装处理

---
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/controller/GraceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.vincent.controller;
+
+import com.vincent.utils.JSONResult;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常拦截处理
+ * 可以针对异常自定义去处理去捕获，返回指定的类型（json类型）到前端
+ */
+@ControllerAdvice
+public class GraceExceptionHandler {
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    @ResponseBody
+    public JSONResult returnMaxFileSizeLimit(FileSizeLimitExceededException e){
+        return JSONResult.errorMsg("文件大小不能超过500KB");
+    }
+}
