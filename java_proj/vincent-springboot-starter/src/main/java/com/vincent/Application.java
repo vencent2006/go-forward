01d495f3b659df1bba160c2f5248b87388e1e922
stayhungry <lewis_sun@foxmail.com>
2022-11-28 01:50:20 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch3-4 使用Mybatis逆向工程生成mapper和pojo

--- java_proj/vincent-springboot-starter/src/main/java/com/vincent/Application.java
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/Application.java
@@ -2,6 +2,7 @@ package com.vincent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * 1. 表明当前项目为SpringBoot工程，这是一个启动类，也是应用程序的入口类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *      扫描完了之后，会放入到spring/springboot的容器中
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.vincent.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/mapper/DbStuMapper.java
@@ -0,0 +1,7 @@
+package com.vincent.mapper;
+
+import com.vincent.my.mapper.MyMapper;
+import com.vincent.pojo.DbStu;
+
+public interface DbStuMapper extends MyMapper<DbStu> {
+}
