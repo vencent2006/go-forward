ef9421ec46c68e05747646d05a4fc7528e64d552
stayhungry <lewis_sun@foxmail.com>
2022-11-30 00:45:13 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch3-8 Hibernate校验参数扩展

--- java_proj/vincent-springboot-starter/src/main/java/com/vincent/pojo/bo/DbStuBO.java
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/pojo/bo/DbStuBO.java
@@ -3,9 +3,11 @@ package com.vincent.pojo.bo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
+import java.util.List;
 
 
 // BO means: business object
@@ -19,4 +21,20 @@ public class DbStuBO {
     private String name;
     @NotNull
     private Integer sex;
+
+    @Min(value = 1, message = "学生的年级最小为1年级")
+    @Max(value = 6, message = "学生的年级最小为6年级")
+    private Integer grade;
+
+    @Range(min = 1, max = 18, message = "学生的所在班级区间为1~18")
+    private Integer classroom;
+
+    @Size(min = 2, max = 5, message = "学生的技能填写至少2个，至多5个")
+    private List<String> skill;
+
+    @Length(min = 3, max = 18, message = "学生的英文名，长度在3~18之间")
+    private String englishName;
+
+    @Email(message = "邮箱格式不正确")
+    private String email;
 }
