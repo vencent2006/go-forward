6a0007366c87f34ba8fd9478434b1a12cb1c79bb
stayhungry <lewis_sun@foxmail.com>
2022-12-01 21:15:50 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch5-2 SpringBoot整合Junit5进行单元测试

---
+++ java_proj/vincent-springboot-starter/src/test/java/com/vincent/test/MyTest.java
@@ -0,0 +1,25 @@
+package com.vincent.test;
+
+import com.vincent.pojo.DbStu;
+import com.vincent.service.StuService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest // 表示当前会被springboot加载，加载为测试类
+public class MyTest {
+    @Autowired
+    private StuService stuService;
+
+    @Test
+    public void testSaveStu() {
+        DbStu stu = new DbStu();
+        stu.setId(UUID.randomUUID().toString());
+        stu.setName("test");
+        stu.setSex(1);
+        stuService.saveStu(stu);
+        // int a = 1/0;
+    }
+}
