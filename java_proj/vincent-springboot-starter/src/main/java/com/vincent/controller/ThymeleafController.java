a5b8d1f14a4231259a6e09148ba8db61e4d20955
stayhungry <lewis_sun@foxmail.com>
2022-12-01 19:09:44 +0800

    imooc: SpringBoot 2.× 手把手零基础入门与进阶 ch4-4 Thymeleaf 循环遍历list和map

--- java_proj/vincent-springboot-starter/src/main/java/com/vincent/controller/ThymeleafController.java
+++ java_proj/vincent-springboot-starter/src/main/java/com/vincent/controller/ThymeleafController.java
@@ -6,8 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @Slf4j
@@ -24,6 +23,24 @@ public class ThymeleafController {
 
         Integer sex = 2;// 0 female; 1 male; 2 unknown
         model.addAttribute("sex", sex);
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Java");
+        myList.add("HTML");
+        myList.add("大数据");
+        myList.add("SpringBoot");
+        myList.add("微服务");
+
+        Map<String, Object> myMap = new HashMap<>();
+        myMap.put("id", 10010);
+        myMap.put("name", "Jack");
+        myMap.put("sex", "男");
+        myMap.put("loves", myList);
+
+        model.addAttribute("myList", myList);
+        model.addAttribute("myMap", myMap);
+
+
 
         return "teacher";
     }
