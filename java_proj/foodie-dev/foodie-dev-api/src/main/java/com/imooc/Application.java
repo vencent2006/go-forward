de6021457c2330242fda912dfc74230c6edc9428
stayhungry <lewis_sun@foxmail.com>
2022-12-20 02:34:51 +0800

    Java架构师-技术专家: 2-22 基于通用Mapper基于Rest编写api接口-1

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,8 +2,11 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 扫描 mybatis 通用mapper所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
