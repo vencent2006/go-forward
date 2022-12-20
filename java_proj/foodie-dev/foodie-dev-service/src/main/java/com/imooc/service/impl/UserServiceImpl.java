650da56f7f07edfd3ceb8116504c0d3458b47eb7
stayhungry <lewis_sun@foxmail.com>
2022-12-21 00:52:35 +0800

    Java架构师-技术专家: 4-1 详细讲解注册登录流程 add @Service 注解给UserServiceImpl

--- java_proj/foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ java_proj/foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
 import com.imooc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UsersMapper usersMapper;
