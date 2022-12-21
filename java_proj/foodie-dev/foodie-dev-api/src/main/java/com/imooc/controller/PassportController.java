b9ce4a84c2d09e547ff3b5001c6e34d8a7da964b
stayhungry <lewis_sun@foxmail.com>
2022-12-21 09:36:50 +0800

    Java架构师-技术专家: 4-2 自定义响应数据结构

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,6 +1,7 @@
 package com.imooc.controller;
 
 import com.imooc.service.UserService;
+import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,19 +16,19 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username) {
+    public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
         // 1. 判断用户名不能为空
         if (StringUtils.isBlank(username)) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
         }
 
         // 2. 查找注册的用户名是否存在
         boolean isExist = userService.queryUsernameIsExist(username);
         if (isExist) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名已经存在");
         }
 
         // 3. 请求成功，用户名没有重复
-        return 200;
+        return IMOOCJSONResult.ok();
     }
 }
