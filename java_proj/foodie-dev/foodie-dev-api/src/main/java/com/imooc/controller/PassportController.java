c42adfb0e925f6408c874fd533b12faacce5cd22
stayhungry <lewis_sun@foxmail.com>
2022-12-21 16:25:47 +0800

    Java架构师-技术专家: 4-8 优化swagger2显示

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -3,16 +3,21 @@ package com.imooc.controller;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "注册登录", tags = {"用于注册登录的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassportController {
     @Autowired
     private UserService userService;
 
+
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
         // 1. 判断用户名不能为空
@@ -30,7 +35,7 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
-
+    @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("regist")
     public IMOOCJSONResult register(@RequestBody UserBO userBO) {
         String username = userBO.getUsername();
