77deb1a930468d16af6c4e704afe3496b357f23c
stayhungry <lewis_sun@foxmail.com>
2022-12-21 22:10:07 +0800

    Java架构师-技术专家: 4-14 整合log4j打印日志

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
     public Object hello() {
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
         return "Hello World!~~~~~~";
     }
 }
