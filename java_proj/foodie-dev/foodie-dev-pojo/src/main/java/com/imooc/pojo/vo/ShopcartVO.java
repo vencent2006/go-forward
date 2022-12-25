dab82ad77280376e7dd3aa8c00706d7d944a22a6
stayhungry <lewis_sun@foxmail.com>
2022-12-25 22:01:23 +0800

    Java架构师-技术专家(第2周): 5-4 购物车 - 渲染（刷新）购物车 - 下

---
+++ java_proj/foodie-dev/foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/ShopcartVO.java
@@ -0,0 +1,90 @@
+package com.imooc.pojo.vo;
+
+public class ShopcartVO {
+    private String ItemId;
+    private String itemImgUrl;
+    private String itemName;
+    private String specId;
+    private String specName;
+    private Integer buyCounts;
+    private String priceDiscount;
+    private String priceNormal;
+
+    public String getItemId() {
+        return ItemId;
+    }
+
+    public void setItemId(String itemId) {
+        ItemId = itemId;
+    }
+
+    public String getItemImgUrl() {
+        return itemImgUrl;
+    }
+
+    public void setItemImgUrl(String itemImgUrl) {
+        this.itemImgUrl = itemImgUrl;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getSpecId() {
+        return specId;
+    }
+
+    public void setSpecId(String specId) {
+        this.specId = specId;
+    }
+
+    public String getSpecName() {
+        return specName;
+    }
+
+    public void setSpecName(String specName) {
+        this.specName = specName;
+    }
+
+    public Integer getBuyCounts() {
+        return buyCounts;
+    }
+
+    public void setBuyCounts(Integer buyCounts) {
+        this.buyCounts = buyCounts;
+    }
+
+    public String getPriceDiscount() {
+        return priceDiscount;
+    }
+
+    public void setPriceDiscount(String priceDiscount) {
+        this.priceDiscount = priceDiscount;
+    }
+
+    public String getPriceNormal() {
+        return priceNormal;
+    }
+
+    public void setPriceNormal(String priceNormal) {
+        this.priceNormal = priceNormal;
+    }
+
+    @Override
+    public String toString() {
+        return "ShopcartBO{" +
+                "ItemId='" + ItemId + '\'' +
+                ", itemImgUrl='" + itemImgUrl + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", specId='" + specId + '\'' +
+                ", specName='" + specName + '\'' +
+                ", buyCounts=" + buyCounts +
+                ", priceDiscount='" + priceDiscount + '\'' +
+                ", priceNormal='" + priceNormal + '\'' +
+                '}';
+    }
+}
