c38d02c12b4bef334dfa9a8d7453c037d93a9d09
stayhungry <lewis_sun@foxmail.com>
2022-12-25 22:16:37 +0800

    Java架构师-技术专家(第2周): 5-6 购物车 - 删除商品业务讲解

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/ShopCartController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/ShopCartController.java
@@ -1,16 +1,13 @@
 package com.imooc.controller;
 
 import com.imooc.pojo.bo.ShopcartBO;
-import com.imooc.service.ItemService;
 import com.imooc.utils.IMOOCJSONResult;
-import com.imooc.utils.PagedGridResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,8 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 public class ShopCartController {
     static final Logger logger = LoggerFactory.getLogger(ShopCartController.class);
 
-    @Autowired
-    private ItemService itemService;
 
     @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车", httpMethod = "POST")
     @PostMapping("/add")
@@ -42,6 +37,30 @@ public class ShopCartController {
         logger.info("shopcartBO is {}", shopcartBO);
 
         // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+
+        return IMOOCJSONResult.ok();
+    }
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "添加商品到购物车", httpMethod = "POST")
+    @PostMapping("/del")
+    public IMOOCJSONResult del(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "itemSpecId", value = "规格id", required = true)
+            @RequestBody String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (StringUtils.isBlank(userId)) {
+            return IMOOCJSONResult.errorMsg("用户id为空");
+        }
+
+        if (StringUtils.isBlank(itemSpecId)) {
+            return IMOOCJSONResult.errorMsg("规格id为空");
+        }
+
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
 
         return IMOOCJSONResult.ok();
     }
