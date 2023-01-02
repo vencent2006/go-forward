cd6abca3449c36c8936cb9ec7c41cc567054e2e3
stayhungry <lewis_sun@foxmail.com>
2023-01-02 12:05:02 +0800

    Java架构师-技术专家(第3周): 3-4 创建订单 - 测试订单创建与回滚

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -2,12 +2,14 @@ package com.imooc.controller;
 
 import com.imooc.enums.PayMethod;
 import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.service.OrderService;
 import com.imooc.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrdersController extends BaseController {
 
     static final Logger logger = LoggerFactory.getLogger(OrdersController.class);
+
+    @Autowired
+    private OrderService orderService;
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
@@ -38,6 +43,8 @@ public class OrdersController extends BaseController {
         2. 创建订单以后，移除购物车中已结算（已提交）的商品
         3. 向支付中心发送当前订单，用于保存支付中心的订单数据
          */
+
+        orderService.createOrder(submitOrderBO);
 
         return IMOOCJSONResult.ok();
     }
