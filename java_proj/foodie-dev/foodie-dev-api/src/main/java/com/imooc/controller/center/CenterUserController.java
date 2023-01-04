1ff949704dfb74831687d14fc860a3ecf6768119
stayhungry <lewis_sun@foxmail.com>
2023-01-04 14:39:45 +0800

    Java架构师-技术专家(第4周): 1-4 用户中心-使用Hibernate验证用户信息

--- java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ java_proj/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -11,10 +11,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "用户信息接口", tags = {"用户信息的相关接口"})
 @RestController
@@ -29,10 +35,17 @@ public class CenterUserController {
     public IMOOCJSONResult update(
             @ApiParam(name = "userId", value = "用户id", required = true)
             @RequestParam String userId,
-            @RequestBody CenterUserBO centerUserBO,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
             HttpServletRequest request,
             HttpServletResponse response
             ) {
+
+        // 判断BindingResult是否保存错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
 
         Users userResult = centerUserService.updateUserInfo(userId, centerUserBO);
         userResult = setNullProperty(userResult);
@@ -43,6 +56,23 @@ public class CenterUserController {
         // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
 
         return IMOOCJSONResult.ok(userResult);
+    }
+
+
+    private Map<String, String> getErrors(BindingResult result) {
+
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error : errorList) {
+            // 发生验证错误所对应的某一个属性
+            String errorField = error.getField();
+            // 验证错误的信息
+            String errorMsg = error.getDefaultMessage();
+
+            map.put(errorField, errorMsg);
+        }
+
+        return map;
     }
 
 
