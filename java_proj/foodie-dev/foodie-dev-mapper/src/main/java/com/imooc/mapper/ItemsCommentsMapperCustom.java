cde6bc9839e852e49acd64acad5b863d8987e7a2
stayhungry <lewis_sun@foxmail.com>
2023-01-18 16:27:53 +0800

    Java架构师-技术专家(第4周): 4-5 评价管理 - 历史评价列表mapper

--- java_proj/foodie-dev/foodie-dev-mapper/src/main/java/com/imooc/mapper/ItemsCommentsMapperCustom.java
+++ java_proj/foodie-dev/foodie-dev-mapper/src/main/java/com/imooc/mapper/ItemsCommentsMapperCustom.java
@@ -1,8 +1,15 @@
 package com.imooc.mapper;
 
+import com.imooc.pojo.vo.MyCommentVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 import java.util.Map;
 
 public interface ItemsCommentsMapperCustom {
 
     public void saveComments(Map<String, Object> map);
+
+    public List<MyCommentVO> queryMyComments(@Param("paramsMap") Map<String, Object> map);
+
 }
