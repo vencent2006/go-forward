86cc1fe2144201c196a7f310e80a5258b9b03c07
stayhungry <lewis_sun@foxmail.com>
2023-04-17 11:52:28 +0800

    exercise hand write errgroup

---
+++ example/errgroup/free_rewrite/errgroup.go
@@ -0,0 +1,96 @@
+package errgroup
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+type token struct{}
+
+type Group struct {
+	wg sync.WaitGroup
+
+	cancel func()
+	sem    chan token
+
+	err     error
+	errOnce sync.Once
+}
+
+func (g *Group) done() {
+	if g.sem != nil {
+		<-g.sem
+	}
+	g.wg.Done()
+}
+
+func (g *Group) Go(f func() error) {
+	if g.sem != nil {
+		g.sem <- token{}
+	}
+
+	g.wg.Add(1)
+	go func() {
+		defer g.done()
+
+		if err := f(); err != nil {
+			g.errOnce.Do(func() {
+				if g.cancel != nil {
+					g.cancel()
+				}
+				g.err = err
+			})
+		}
+	}()
+}
+
+func (g *Group) TryGo(f func() error) bool {
+	if g.sem != nil {
+		select {
+		case g.sem <- token{}:
+		default:
+			return false
+		}
+	}
+
+	g.wg.Add(1)
+	go func() {
+		defer g.done()
+		if err := f(); err != nil {
+			g.errOnce.Do(func() {
+				if g.cancel != nil {
+					g.cancel()
+				}
+				g.err = err
+			})
+		}
+	}()
+	return true
+}
+
+func (g *Group) Wait() error {
+	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel()
+	}
+	return g.err
+}
+
+func (g *Group) SetLimit(n int) {
+	if n < 0 {
+		g.sem = nil
+		return
+	}
+
+	if len(g.sem) != 0 {
+		panic(fmt.Errorf("errgroup: modify limit while %v goroutines still active", len(g.sem)))
+	}
+
+	g.sem = make(chan token, n)
+}
+
+func WithContext(ctx context.Context) (*Group, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	return &Group{cancel: cancel}, ctx
+}
