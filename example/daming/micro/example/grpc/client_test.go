d8bf835863d4512f33ae97830ac80b2951bc3964
vencent2006 <lewis_sun@foxmail.com>
2023-05-05 10:09:50 +0800

    daming| week 14: 3.服务注册与发现: 以gRPC为底层通信协议

--- example/daming/micro/example/grpc/client_test.go
+++ example/daming/micro/example/grpc/client_test.go
@@ -1,1 +1,23 @@
 package grpc
+
+import (
+	"context"
+	"example/daming/micro/proto/gen"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"testing"
+	"time"
+)
+
+func TestClient(t *testing.T) {
+	// grpc.WithInsecure, 表示不使用证书(https)
+	cc, err := grpc.Dial("localhost:8081", grpc.WithInsecure())
+	require.NoError(t, err)
+	client := gen.NewUserServiceClient(cc)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	resp, err := client.GetById(ctx, &gen.GetByIdReq{Id: 13})
+	require.NoError(t, err)
+	t.Log(resp)
+
+}
--- example/daming/micro/example/grpc/server_test.go
+++ example/daming/micro/example/grpc/server_test.go
@@ -1,1 +1,34 @@
 package grpc
+
+import (
+	"context"
+	"example/daming/micro/proto/gen"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"net"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	us := &Server{}
+	server := grpc.NewServer()
+	gen.RegisterUserServiceServer(server, us)
+	l, err := net.Listen("tcp", ":8081")
+	require.NoError(t, err)
+	err = server.Serve(l)
+	t.Log(err)
+}
+
+type Server struct {
+	gen.UnimplementedUserServiceServer
+}
+
+func (s Server) GetById(ctx context.Context, req *gen.GetByIdReq) (*gen.GetByIdResp, error) {
+	fmt.Println(req)
+	return &gen.GetByIdResp{
+		User: &gen.User{
+			Name: "hello, world",
+		},
+	}, nil
+}
