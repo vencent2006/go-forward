1cbeb3de77f5f751d5042e232f081e5cddcef6dc
vencent2006 <lewis_sun@foxmail.com>
2023-05-08 12:52:29 +0800

    algo | 0021.merge-two-sorted-lists

---
+++ python_proj/algo/0021.merge-two-sorted-lists/solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
--- python_proj/algo/0021.merge-two-sorted-lists/test_solution.py
+++ python_proj/algo/0021.merge-two-sorted-lists/test_solution.py
@@ -1,8 +1,64 @@
 import unittest
+from solution import *
+from typing import List, Optional
+
+
+def buildListNode(l: List[int]) -> Optional[ListNode]:
+    lastNode = None
+    for i in range(-1, -len(l) - 1, -1):
+        print(l[i])
+        node = ListNode(l[i], lastNode)
+        lastNode = node
+
+    if l is None:
+        return None
+    else:
+        return lastNode
+
+
+def listNode2List(node: ListNode) -> list:
+    l = []
+    while node is not None:
+        l.append(node.val)
+        node = node.next
+    return l
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_case_1(self):
+        list1 = buildListNode([1, 2, 4])
+        list2 = buildListNode([1, 3, 4])
+        res = Solution().mergeTwoLists(list1, list2)
+        wantRes = [1, 1, 2, 3, 4, 4]
+        self.assertEqual(listNode2List(res), wantRes)  # add assertion here
+
+    def test_case_2(self):
+        list1 = buildListNode([])
+        list2 = buildListNode([])
+        res = Solution().mergeTwoLists(list1, list2)
+        wantRes = []
+        self.assertEqual(listNode2List(res), wantRes)  # add assertion here
+
+    def test_case_3(self):
+        list1 = buildListNode([])
+        list2 = buildListNode([0])
+        res = Solution().mergeTwoLists(list1, list2)
+        wantRes = [0]
+        self.assertEqual(listNode2List(res), wantRes)  # add assertion here
+
+    def test_buildListNode(self):
+        l = [1, 2, 3, 4]
+        res = buildListNode(l)
+        print(res)
+
+    def test_listNode2List(self):
+        node1 = ListNode(4, None)
+        node2 = ListNode(3, node1)
+        node3 = ListNode(2, node2)
+        head = ListNode(1, node3)
+        l = listNode2List(head)
+        print(l)
+
 
 if __name__ == '__main__':
     unittest.main()
