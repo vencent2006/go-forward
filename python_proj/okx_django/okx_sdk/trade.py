a9e616b1b1dccbb458367d7d8561977fd6629f5e
vencent2006 <lewis_sun@foxmail.com>
2023-05-27 15:29:58 +0800

    sdk的增加trade的返回值示例

--- python_proj/okx_django/okx_sdk/trade.py
+++ python_proj/okx_django/okx_sdk/trade.py
@@ -15,6 +15,19 @@ def buy_limit_order(instId: str, price, size, clientOrderId=""):
     :param size: 数量
     :param clientOrderId: 自定义订单id，字母（区分大小写）与数字的组合，可以是纯字母、纯数字且长度要在1-32位之间
     :return:
+    {
+        "code":"0",
+        "msg":"",
+        "data":[
+            {
+                "clOrdId":"oktswap6",
+                "ordId":"12345689",
+                "tag":"",
+                "sCode":"0",
+                "sMsg":""
+            }
+        ]
+    }
     """
 
     result = tradeAPI.place_order(
@@ -74,6 +87,20 @@ def sell_limit_order(instId: str, price, size, clientOrderId=""):
     :param size: 数量
     :param clientOrderId: 自定义订单id，字母（区分大小写）与数字的组合，可以是纯字母、纯数字且长度要在1-32位之间
     :return:
+    {
+        "code":"0",
+        "msg":"",
+        "data":[
+            {
+                "clOrdId":"oktswap6",
+                "ordId":"12345689",
+                "tag":"",
+                "sCode":"0",
+                "sMsg":""
+            }
+        ]
+    }
+
     """
 
     result = tradeAPI.place_order(
