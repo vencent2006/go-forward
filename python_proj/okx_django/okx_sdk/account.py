c4ae94c3a3a77019f0ff5a62d6f909157a997952
vencent2006 <lewis_sun@foxmail.com>
2023-05-27 15:43:25 +0800

    sdk的account balance的返回值示例

--- python_proj/okx_django/okx_sdk/account.py
+++ python_proj/okx_django/okx_sdk/account.py
@@ -28,6 +28,54 @@ def get_one_account_balance(ccy):
     result = accountAPI.get_account_balance(ccy)
     print_pretty_json(result)
     return result
+    """
+    {
+      "code": "0",
+      "data": [
+        {
+          "adjEq": "",
+          "details": [
+            {
+              "availBal": "8604.73203",
+              "availEq": "",
+              "cashBal": "8984.73203",
+              "ccy": "USDT",
+              "crossLiab": "",
+              "disEq": "8986.331042094142",
+              "eq": "8985.342654402157",
+              "eqUsd": "8986.331042094142",
+              "fixedBal": "0",
+              "frozenBal": "380.6106244021568",
+              "interest": "",
+              "isoEq": "",
+              "isoLiab": "",
+              "isoUpl": "",
+              "liab": "",
+              "maxLoan": "",
+              "mgnRatio": "",
+              "notionalLever": "",
+              "ordFrozen": "380",
+              "spotInUseAmt": "",
+              "stgyEq": "0.6106244021568",
+              "twap": "0",
+              "uTime": "1684921492973",
+              "upl": "",
+              "uplLiab": ""
+            }
+          ],
+          "imr": "",
+          "isoEq": "",
+          "mgnRatio": "",
+          "mmr": "",
+          "notionalUsd": "",
+          "ordFroz": "",
+          "totalEq": "187794.6998519608",
+          "uTime": "1684979659450"
+        }
+      ],
+      "msg": ""
+    }
+    """
 
 
 if __name__ == '__main__':
