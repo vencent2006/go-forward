db188d914b90db6d9e3401edd1c4e05dc8b90e4d
vencent2006 <lewis_sun@foxmail.com>
2023-07-17 07:30:41 +0800

    example | init in test file

---
+++ example/init_in_test/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("************ run init in test file ************")
+	fmt.Println("************ run init in test file ************")
+	fmt.Println("************ run init in test file ************")
+	fmt.Println("************ run init in test file ************")
+	fmt.Println("************ run init in test file ************")
+}
