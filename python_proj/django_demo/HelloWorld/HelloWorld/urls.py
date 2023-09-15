ee557dcd7da57b88ec04e7159a9245e11bbadd7d
vencent2006 <lewis_sun@foxmail.com>
2023-09-15 15:34:48 +0800

    get 提交表单

--- python_proj/django_demo/HelloWorld/HelloWorld/urls.py
+++ python_proj/django_demo/HelloWorld/HelloWorld/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 
-from . import views
-from . import testdb
+
+from . import views, testdb, search
 
 urlpatterns = [
     path('runoob/', views.runoob),
     path('testdb/', testdb.testdb),
+    url(r'^search-form/$', search.search_form),
+    url(r'^search/$', search.search),
 ]
