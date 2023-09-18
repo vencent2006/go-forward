b5a774a55fd18d916f61703d5a23d76035ecf425
vencent2006 <lewis_sun@foxmail.com>
2023-09-18 17:20:54 +0800

    增加显示的内容、搜索框和过滤器

--- python_proj/django_demo/laoqi/blog/admin.py
+++ python_proj/django_demo/laoqi/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BlogArticles
+
+
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publish")
+    list_filter = ("publish", "author")
+    search_fields = ('title', "body")
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ['-publish', 'author']
+
 
 # Register your models here.
+admin.site.register(BlogArticles, BlogArticlesAdmin)
