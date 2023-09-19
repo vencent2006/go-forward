828e31a66eea1358d8898a529a98cda5f048257a
vencent2006 <lewis_sun@foxmail.com>
2023-09-19 18:10:39 +0800

    account login2

--- python_proj/django_demo/laoqi/account/urls.py
+++ python_proj/django_demo/laoqi/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.user_login, name='user_login')
+    # path('login/', views.user_login, name='user_login'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login2.html'), name='user_login')
 ]
--- python_proj/django_demo/laoqi/mysite/settings.py
+++ python_proj/django_demo/laoqi/mysite/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+LOGIN_REDIRECT_URL = '/blog/'
