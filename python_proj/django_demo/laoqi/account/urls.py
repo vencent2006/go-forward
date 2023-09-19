a7029c5290928a8ae0cdef3c46ed4122b67d3a14
vencent2006 <lewis_sun@foxmail.com>
2023-09-19 18:33:22 +0800

    account logout

--- python_proj/django_demo/laoqi/account/urls.py
+++ python_proj/django_demo/laoqi/account/urls.py
@@ -6,5 +6,6 @@ app_name = 'account'
 
 urlpatterns = [
     # path('login/', views.user_login, name='user_login'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login2.html'), name='user_login')
+    path('login/', auth_views.LoginView.as_view(template_name='account/login2.html'), name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='user_logout')
 ]
