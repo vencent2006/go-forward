8089d718b1606d98b8136206531b4d87de8a6217
vencent2006 <lewis_sun@foxmail.com>
2023-09-26 14:29:39 +0800

    原始http调用

--- example/raw_http_server_client/server/server.go
+++ example/raw_http_server_client/server/server.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
+var (
+	Addr = ":1210"
+)
+
+func main() {
+	// 创建路由器
+	mux := http.NewServeMux()
+	// 设置路由规则
+	mux.HandleFunc("/bye", sayBye)
+	// 创建服务器
+	server := &http.Server{
+		Addr:         Addr,
+		WriteTimeout: time.Second * 3,
+		Handler:      mux,
+	}
+	// 监听端口并提供服务
+	log.Println("Starting httpserver at " + Addr)
+	log.Fatal(server.ListenAndServe())
+}
+
+func sayBye(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(1 * time.Second)
+	log.Printf("handle request: %v", r.URL)
+	w.Write([]byte("bye bye ,this is httpServer"))
 }
