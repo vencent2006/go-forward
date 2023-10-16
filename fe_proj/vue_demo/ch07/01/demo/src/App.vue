71e5fdb6979ff0cf798244c4da5cf399521eef8b
vencent2006 <lewis_sun@foxmail.com>
2023-10-16 16:24:18 +0800

    example fe vue ch07-03

--- fe_proj/vue_demo/ch07/01/demo/src/App.vue
+++ fe_proj/vue_demo/ch07/01/demo/src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="inputValue"/>
+    <button class="button" @click="handleAddItem">提交</button>
+  </div>
+  <ul>
+    <list-item
+      v-for="(item, index) in list"
+      :key="index"
+      :msg="item"
+    />
+  </ul>
 </template>
 
 <script>
 // 单文件组件
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from 'vue';
+import ListItem from './components/ListItem.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ListItem },
+  setup() {
+    const inputValue = ref('')
+    const list = reactive([])
+    const handleAddItem = () => {
+      list.push(inputValue.value)
+      inputValue.value = ''
+    }
+    return { inputValue, list, handleAddItem }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .button {
+    margin-left: 20px;
+    color: red;
+  }
 </style>
--- fe_proj/vue_demo/ch07/01/demo/src/components/ListItem.vue
+++ fe_proj/vue_demo/ch07/01/demo/src/components/ListItem.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <li>{{ msg }}</li>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'ListItem',
   props: {
     msg: String
   }
