ce2ce09657809e4e2b75f1ab5ec6142fff0c913a
vencent2006 <lewis_sun@foxmail.com>
2023-10-19 00:32:56 +0800

    example fe vue | scss 安装

--- fe_proj/vue_demo/jingdong/src/main.js
+++ fe_proj/vue_demo/jingdong/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import './style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
