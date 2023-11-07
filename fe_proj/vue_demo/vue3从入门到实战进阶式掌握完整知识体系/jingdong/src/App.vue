2f8d24f81e416c6f538539505bc78a2db11bf3a3
vencent2006 <lewis_sun@foxmail.com>
2023-11-07 18:34:39 +0800

    example fe vue ch8_4

--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/App.vue
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/App.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>123</div>
+  <div class="docker">
+    <!-- BEM命名 block__element--modifier -->
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6f3;</div>
+      <div class="docker__item__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe7e5;</div>
+      <div class="docker__item__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker__item__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__item__title">我的</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  padding: 0 .18rem;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 1px;
+  border-top: 1px solid #F1F1F1;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      font-size: .18rem;
+      margin: .07rem 0 .02rem 0;
+    }
+
+    &--active {
+      color: #1FA4FC;
+    }
+
+    &__title {
+      font-size: .2rem;
+      // 老师说，移动端最小显示像素是12px，如果想显示10px，就需要用transform进行缩放
+      transform: scale(.5, .5);
+      transform-origin: center top;
+    }
+  }
+}
+</style>
