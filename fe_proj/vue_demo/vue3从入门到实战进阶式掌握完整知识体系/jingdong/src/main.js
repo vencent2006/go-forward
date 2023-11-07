0b1dd4925e8ff697b4556527676c48fce1e9abb7
vencent2006 <lewis_sun@foxmail.com>
2023-11-07 16:59:14 +0800

    example fe vue ch8_3

--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/main.js
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 抹平不同浏览器间的差异
+import 'normalize.css'
+import './style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
