7940478efe8d07e7f7f343f0adbe9bfea3d84978
vencent2006 <lewis_sun@foxmail.com>
2023-11-12 22:56:26 +0800

    example fe vue ch9_3

--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/router/index.js
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/home/Home'
 import Login from '../views/login/Login'
+import Register from '../views/register/Register'
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage
+      isLogin ? next({ name: 'Home' }) : next()
+    }
   },
   {
     path: '/login',
@@ -34,7 +44,10 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   // console.log(to, from)
   const { isLogin } = localStorage
-  isLogin || to.name === 'Login' ? next() : next({ name: 'Login' })
+  const { name } = to
+  const isLoginOrRegister = (name === 'Login' || name === 'Register')
+  // 已经登录了 or 访问的是登录|注册页面， 那么可以直接访问；否则就去登录去
+  isLogin || isLoginOrRegister ? next() : next({ name: 'Login' })
 })
 
 export default router
--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/views/register/Register.vue
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/views/register/Register.vue
@@ -7,26 +7,24 @@
     <div class="wrapper__input">
       <input class="wrapper__input__content" placeholder="请输入密码" type="password" />
     </div>
-    <div class="wrapper__login-button" @click="handleLogin">登录</div>
-    <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
+    <div class="wrapper__input">
+      <input class="wrapper__input__content" placeholder="确认密码" type="password" />
+    </div>
+    <div class="wrapper__register-button">注册</div>
+    <div class="wrapper__register-link" @click="handleLoginClick">已有账号去登录</div>
   </div>
 </template>
 
 <script>
 import { useRouter } from 'vue-router'
 export default {
-  name: 'Login',
+  name: 'Register',
   setup() {
     const router = useRouter()
-    const handleLogin = () => {
-      localStorage.isLogin = true
-      // 这个'Home'是router里的名字
-      router.push({ name: 'Home' })
+    const handleLoginClick = () => {
+      router.push({ name: 'Login' })
     }
-    const handleRegisterClick = () => {
-      router.push({ name: 'Register' })
-    }
-    return { handleLogin, handleRegisterClick }
+    return { handleLoginClick }
   }
 }
 </script>
@@ -72,7 +70,7 @@ export default {
     }
   }
 
-  &__login-button {
+  &__register-button {
     margin: .32rem .4rem .16rem .4rem;
     line-height: .48rem;
     background: #0091FF;
@@ -83,7 +81,7 @@ export default {
     text-align: center;
   }
 
-  &__login-link {
+  &__register-link {
     text-align: center;
     font-size: .14rem;
     color: $content-notice-fontcolor;
