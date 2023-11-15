f20f49a7d6487010d538918cbcabe27f9c224e25
vencent2006 <lewis_sun@foxmail.com>
2023-11-15 18:26:47 +0800

    example fe vue ch10_7

--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/views/shop/Shop.vue
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/src/views/shop/Shop.vue
@@ -7,35 +7,53 @@
         <input class="search__content__input" placeholder="请输入商品名称" />
       </div>
     </div>
-    <ShopInfo :item="data.item" :hideBorder="true" />
+    <!-- v-show item.imgUrl有数据了，才展示组件 -->
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl" />
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
-import { useRouter } from 'vue-router'
+import { reactive, toRefs } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import { get } from '@/utils/request'
 import ShopInfo from '@/components/ShopInfo'
-// https://fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/shop/hot-list
+
+const useShopInfoEffect = () => {
+  const data = reactive({ item: {} }) // 记住：这种reactive的方式
+
+  const getItemData = async () => {
+    const route = useRoute()
+    // console.log(route.params.id)
+    const result = await get(`/api/shop/${route.params.id}`)
+    console.log(result)
+    if (result?.errno === 0 && result?.data) {
+      data.item = result.data
+    }
+  }
+
+  const { item } = toRefs(data)
+  return { item, getItemData }
+}
+
+// 点击回退逻辑
+const useBackRouterEffect = () => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    // todo: 如果const router = useRouter()
+    // 放在这里会报错，不知道为啥
+    router.back()
+  }
+  return { handleBackClick }
+}
+
 export default {
   name: 'Shop',
   components: { ShopInfo },
   setup() {
-    const router = useRouter()
-    const data = reactive({ item: {} })
-    const getItemData = async () => {
-      const result = await get('/api/shop/1')
-      console.log(result)
-      if (result?.errno === 0 && result?.data) {
-        data.item = result.data
-      }
-    }
-    getItemData()
-
-    const handleBackClick = () => {
-      router.back()
-    }
-    return { data, handleBackClick }
+    const { item, getItemData } = useShopInfoEffect()
+    const { handleBackClick } = useBackRouterEffect()
+    getItemData() // 执行一下，取下数据
+    return { item, handleBackClick }
   }
 }
 </script>
