1425f326bd1f86f89ba2b450ca3728e3c43b7276
vencent2006 <lewis_sun@foxmail.com>
2023-11-23 12:45:04 +0800

    example fe vue ch13_4 the end

--- fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/vue.config.js
+++ fe_proj/vue_demo/vue3从入门到实战进阶式掌握完整知识体系/jingdong/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 这个publicPath 要具体看你前端代码访问的路径是什么
+  // 比如前端代码在域名访问的时候，还需要加个jingdong，那么publicPath:'/jingdong'
+  // 这个是配置静态资源访问的路径的，当然这些可以nginx的conf进行配置，让前端配置尽可能简单
+  publicPath: './'
 })
