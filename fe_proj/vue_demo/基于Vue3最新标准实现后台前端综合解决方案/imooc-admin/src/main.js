016862a5e0be0a002e01c3f6103da4eabb606307
vencent2006 <lewis_sun@foxmail.com>
2023-11-28 17:50:25 +0800

    example fe vue3 admin ch3_8

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/main.js
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+// 初始化样式表
+import '@/styles/index.scss'
 
 const app = createApp(App)
 installElementPlus(app)
--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/views/login/IndexView.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/views/login/IndexView.vue
@@ -2,6 +2,7 @@
   <div class="login-container">
     <el-form class="login-form">
       <div class="title-container">
+        <h3 class="title">bluev admin</h3>
         <h3 class="title">用户登录</h3>
       </div>
       <!-- username -->
@@ -40,4 +41,75 @@
 import { Avatar } from '@element-plus/icons'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        caret-color: $cursor; // 光标颜色
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
