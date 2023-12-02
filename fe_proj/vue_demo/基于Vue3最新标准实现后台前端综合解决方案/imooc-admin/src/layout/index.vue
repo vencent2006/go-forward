5abf2d2a0f6a52fb0d1eabd440a656d2eb7bcee0
vencent2006 <lewis_sun@foxmail.com>
2023-12-02 23:18:25 +0800

    example fe vue3 admin | variables.scss rename

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/index.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/index.vue
@@ -1,28 +1,35 @@
 <template>
   <div class="app-wrapper">
     <!-- 左侧 menu -->
-    <sidebar class="sidebar-container"></sidebar>
+    <!-- <sidebar class="sidebar-container"></sidebar> -->
     <div class="main-container">
       <div class="fixed-header">
         <!-- 顶部 navbar -->
-        <navbar />
+        <!-- <navbar /> -->
       </div>
       <!-- 内容区 -->
-      <app-main></app-main>
+      <!-- <app-main></app-main> -->
     </div>
+    <!-- test -->
+    <!-- <div><h1 :style="{ color: color }">JS 获取 SCSS 变量值</h1></div> -->
   </div>
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Sidebar from './components/Sidebar' // 因为是index.vue，所以可以省略
-import AppMain from './components/AppMain.vue'
-import {} from 'vue'
+// import Navbar from './components/Navbar.vue'
+// import Sidebar from './components/Sidebar' // 因为是index.vue，所以可以省略
+// import AppMain from './components/AppMain.vue'
+import variables from '@/styles/variables.module.scss'
+import { ref } from 'vue'
+
+console.log(variables)
+const color = ref(variables.menuText)
+console.log(color)
 </script>
 
 <style lang="scss" scoped>
 @import '~@/styles/mixin.scss';
-@import '~@/styles/variables.scss';
+@import '~@/styles/variables.module.scss';
 
 .app-wrapper {
   @include clearfix;
