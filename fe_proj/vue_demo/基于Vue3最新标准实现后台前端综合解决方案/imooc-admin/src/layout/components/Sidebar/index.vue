b36309d461d875e727b21c6ee4e524305d1ea11c
vencent2006 <lewis_sun@foxmail.com>
2023-12-04 17:09:42 +0800

    example fe vue3 admin ch4-10: 创建页面组件，使用临时 menu 菜单

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/index.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/index.vue
@@ -1,7 +1,14 @@
-<template><div>menu</div></template>
+<template>
+  <div class="">
+    <h1>占位</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
+</template>
 
 <script setup>
+import SidebarMenu from './SidebarMenu'
 import {} from 'vue'
 </script>
-
 <style lang="scss" scoped></style>
