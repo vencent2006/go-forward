af3cf0497d95dfe1b0b38a6ff6de816b958d348e
vencent2006 <lewis_sun@foxmail.com>
2023-12-05 18:12:28 +0800

    example fe vue3 admin ch4-14: 业务落地：生成动态 menu 菜单

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/SidebarMenu.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/SidebarMenu.vue
@@ -7,26 +7,18 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu 菜单 -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 <script setup>
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
+import SidebarItem from './SidebarItem'
 
 const router = useRouter()
 console.log('router.getRoutes()')
