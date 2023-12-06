3a437ac5fd7891219ed4d60215c65ab49b4ce802
vencent2006 <lewis_sun@foxmail.com>
2023-12-06 15:53:51 +0800

    example fe vue3 admin ch4-17： SidebarHeader 处理

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/index.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/Sidebar/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="">
-    <h1>占位</h1>
+    <div class="logo-container">
+      <el-avatar
+        size="44"
+        shape="square"
+        src="https://img2.baidu.com/it/u=648933335,1401463189&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400"
+      />
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">admin</h1>
+    </div>
     <el-scrollbar>
       <sidebar-menu></sidebar-menu>
     </el-scrollbar>
@@ -11,4 +18,20 @@
 import SidebarMenu from './SidebarMenu'
 import {} from 'vue'
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
