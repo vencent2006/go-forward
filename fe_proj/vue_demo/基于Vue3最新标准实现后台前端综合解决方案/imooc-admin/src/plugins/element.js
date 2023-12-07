b906aaa7a6105ae6157ce9f885e051406b1a9175
vencent2006 <lewis_sun@foxmail.com>
2023-12-07 15:33:10 +0800

    example fe vue3 admin ch5-05：方案落地：element-plus 国际化处理

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/plugins/element.js
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/plugins/element.js
@@ -1,7 +1,10 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
-
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
+import store from '@/store'
 export default (app) => {
-  app.use(ElementPlus, { locale })
+  // 注册 `element` 时，根据当前语言选择使用哪种语言包
+  // todo 会随着 store.getters.language 变化来导入不同的包吗？
+  app.use(ElementPlus, { locale: store.getters.language === 'en' ? en : zhCn })
 }
