a8d7217072b9e8e5a779e2e6bcb8a570bae594c4
vencent2006 <lewis_sun@foxmail.com>
2023-12-15 16:48:04 +0800

    solidity ch05

---
+++ block_chain_proj/imooc_web3_nft_shizhan/ch05/AbiInterface.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+ 
+interface AbiInterface{
+    function setX(uint _x) external returns(uint);
+}
---
+++ block_chain_proj/imooc_web3_nft_shizhan/ch05/IERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+ 
+ interface IERC20{
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+}
+
+
+
+
+// OxdAC17F958D2ee523a2206206994597C13D831ec7
