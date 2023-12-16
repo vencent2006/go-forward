7001d0a845efc1c9a9e0aada7e597b59aa5b3ad1
vencent2006 <lewis_sun@foxmail.com>
2023-12-16 16:52:21 +0800

    example fe vue3 admin ch5-37：方案落地：处理基于路由的动态过渡

--- fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/AppMain.vue
+++ fe_proj/vue_demo/基于Vue3最新标准实现后台前端综合解决方案/imooc-admin/src/layout/components/AppMain.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app-main">
-    <router-view></router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade-transform" mode="out-in">
+        <keep-alive>
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -70,11 +76,11 @@ watchSwitchLang(() => {
 <style lang="scss" scoped>
 .app-main {
   // 浏览器可视区域的高度 100vh
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   width: 100%;
   position: relative;
   overflow: hidden;
-  padding: 61px 20px 20px 20px;
+  padding: 104px 20px 20px 20px;
   box-sizing: border-box;
 }
 </style>
