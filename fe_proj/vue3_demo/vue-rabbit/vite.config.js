09edd52a0153b9832c3555af24f158dcbefabc25
vencent2006 <lewis_sun@foxmail.com>
2024-01-13 15:48:06 +0800

    elementPlus 主题色定制

--- fe_proj/vue3_demo/vue-rabbit/vite.config.js
+++ fe_proj/vue3_demo/vue-rabbit/vite.config.js
@@ -17,13 +17,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+          // 1. 配置elementPlus采用sass样式配色系统
+          ElementPlusResolver({importStyle:"sass"})
+      ],
     }),
     // elementPlus 按需导入 | end
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
