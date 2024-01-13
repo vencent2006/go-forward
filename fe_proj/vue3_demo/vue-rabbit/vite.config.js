7b71ff55dedcb66bf8ec58b65e09a533f10ec38a
vencent2006 <lewis_sun@foxmail.com>
2024-01-13 14:48:09 +0800

    elementPlus 按需导入配置

--- fe_proj/vue3_demo/vue-rabbit/vite.config.js
+++ fe_proj/vue3_demo/vue-rabbit/vite.config.js
@@ -3,10 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// elementPlus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // elementPlus 按需导入 | start
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // elementPlus 按需导入 | end
   ],
   resolve: {
     alias: {
