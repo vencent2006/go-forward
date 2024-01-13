8086c1a21d5e907356b26e6e700bcf6943a6884d
vencent2006 <lewis_sun@foxmail.com>
2024-01-13 18:03:37 +0800

    2-12 scss文件的自动导入

--- fe_proj/vue3_demo/vue-rabbit/src/App.vue
+++ fe_proj/vue3_demo/vue-rabbit/src/App.vue
@@ -4,9 +4,16 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView/>
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
--- fe_proj/vue3_demo/vue-rabbit/vite.config.js
+++ fe_proj/vue3_demo/vue-rabbit/vite.config.js
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // elementPlus 按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [
-          // 1. 配置elementPlus采用sass样式配色系统
-          ElementPlusResolver({importStyle:"sass"})
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({importStyle: "sass"})
       ],
     }),
     // elementPlus 按需导入 | end
@@ -32,9 +32,10 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        // 自动导入定制化样式文件进行样式覆盖
+        // 2. 自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
         `,
       }
     }
