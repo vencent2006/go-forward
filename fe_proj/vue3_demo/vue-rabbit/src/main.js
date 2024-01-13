40c7136f4fa65e3eb4a99be250f10e5b4e0970d6
vencent2006 <lewis_sun@foxmail.com>
2024-01-13 17:51:03 +0800

    2-11 静态资源引入

--- fe_proj/vue3_demo/vue-rabbit/src/main.js
+++ fe_proj/vue3_demo/vue-rabbit/src/main.js
@@ -1,15 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+// 引入初始化样式文件
+import '@/styles/common.scss'
+
 // 测试
 import {getCategory} from "@/apis/testAPI"
-getCategory().then(res=>{
-    console.log(res)
+
+getCategory().then(res => {
+  console.log(res)
 })
 
 const app = createApp(App)
