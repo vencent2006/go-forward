356937d6b7da84b2b793f00404b8c3f351458a07
vencent2006 <lewis_sun@foxmail.com>
2024-01-13 18:27:28 +0800

    2-13 layout-静态结构搭建

--- fe_proj/vue3_demo/vue-rabbit/src/views/Layout/index.vue
+++ fe_proj/vue3_demo/vue-rabbit/src/views/Layout/index.vue
@@ -1,9 +1,12 @@
-<template>
-  <div>
-    我是首页
-    <!--  二级路由出口  -->
-    <RouterView/>
-  </div>
-</template>
 <script setup>
+import LayoutNav from "@/views/Layout/components/LayoutNav.vue";
+import LayoutHeader from "@/views/Layout/components/LayoutHeader.vue";
+import LayoutFooter from "@/views/Layout/components/LayoutFooter.vue";
 </script>
+
+<template>
+  <LayoutNav/>
+  <LayoutHeader/>
+  <RouterView/>
+  <LayoutFooter/>
+</template>
