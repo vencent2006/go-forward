bdc543683ec6e38210b5164ba141d2d547e21848
vencent2006 <lewis_sun@foxmail.com>
2024-02-03 23:08:10 +0800

    feat[layout]: 1.  安装 vueUse 2. 吸顶导航的实现

--- fe_proj/vue3_demo/vue-rabbit/src/views/Layout/index.vue
+++ fe_proj/vue3_demo/vue-rabbit/src/views/Layout/index.vue
@@ -2,8 +2,10 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFiexed from './components/LayoutFiexed.vue'
 </script>
 <template>
+  <LayoutFiexed />
   <LayoutNav />
   <LayoutHeader />
   <!-- 二级路由出口 -->
