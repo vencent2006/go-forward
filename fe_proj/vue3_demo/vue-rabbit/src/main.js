439e0b5997e639a837efdd270700cd77ce64da52
vencent2006 <lewis_sun@foxmail.com>
2024-02-03 22:53:26 +0800

    fix[main.js]: 删除无用的测试代码

--- fe_proj/vue3_demo/vue-rabbit/src/main.js
+++ fe_proj/vue3_demo/vue-rabbit/src/main.js
@@ -6,12 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// 测试接口函数
-import { getCategory } from '@/apis/testAPI'
-getCategory().then((res) => {
-  console.log(res)
-})
-
 const app = createApp(App)
 
 app.use(createPinia())
