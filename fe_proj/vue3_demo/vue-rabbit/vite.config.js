29c2e112d17590c388683085a011f2282c16a81f
vencent2006 <lewis_sun@foxmail.com>
2024-02-03 17:12:03 +0800

    feat[path alias]: 别名联想配置

--- fe_proj/vue3_demo/vue-rabbit/vite.config.js
+++ fe_proj/vue3_demo/vue-rabbit/vite.config.js
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
+    // 别名路径配置项 实际的路径转换 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
