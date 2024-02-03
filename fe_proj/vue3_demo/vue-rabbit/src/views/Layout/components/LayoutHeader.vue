83d9ef322080077874debce2924662bcb778cb79
vencent2006 <lewis_sun@foxmail.com>
2024-02-03 22:52:21 +0800

    feat[layout]: 渲染layout的一级导航列表

--- fe_proj/vue3_demo/vue-rabbit/src/views/Layout/components/LayoutHeader.vue
+++ fe_proj/vue3_demo/vue-rabbit/src/views/Layout/components/LayoutHeader.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { getCategoryAPI } from '@/apis/layout'
+import { onMounted, ref } from 'vue'
+
+const categoryList = ref([])
+const getCategory = async () => {
+  const res = await getCategoryAPI()
+  console.log(res)
+  categoryList.value = res.result
+}
+
+onMounted(() => {
+  getCategory()
+})
+</script>
 
 <template>
   <header class="app-header">
@@ -7,12 +21,9 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <li class="home" v-for="item in categoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li><RouterLink to="/">居家</RouterLink></li>
-        <li><RouterLink to="/">美食</RouterLink></li>
-        <li><RouterLink to="/">服饰</RouterLink></li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
