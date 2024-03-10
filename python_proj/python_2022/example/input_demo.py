5e9d9d162098ea010133012f7c3fc201864ec3c4
vencent2006 <lewis_sun@foxmail.com>
2024-03-10 22:45:27 +0800

    python demo: list append

--- python_proj/python_2022/example/input_demo.py
+++ python_proj/python_2022/example/input_demo.py
@@ -1,4 +1,8 @@
-m = 0
+list = []
 for i in range(5):
-    m += int(input())
-print(m)
+    list.append(int(input()))
+for i in list:
+    if i == 6 or i == 8:
+        print('幸运数字')
+    else:
+        print('普通数字')
