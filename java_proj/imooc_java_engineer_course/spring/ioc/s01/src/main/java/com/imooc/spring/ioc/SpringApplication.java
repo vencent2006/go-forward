126c152fe7669b28e42329a7efcc4ffd3474b889
vencent2006 <lewis_sun@foxmail.com>
2024-03-11 09:51:34 +0800

    imooc java工程师: 3-1 SpringIoc初体验-1

---
+++ java_proj/imooc_java_engineer_course/spring/ioc/s01/src/main/java/com/imooc/spring/ioc/SpringApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.spring.ioc;
+
+import com.imooc.spring.ioc.entity.Apple;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        // 初始化ioc容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        Apple sweetApple = context.getBean("sweetApple", Apple.class);
+        System.out.println(sweetApple.getTitle());
+    }
+}
