1bb20caf3088ff081c82a04ee15f2000789a9bae
vencent2006 <lewis_sun@foxmail.com>
2024-03-16 09:52:35 +0800

    xml配置bean

--- java_proj/imooc_java_engineer_course/spring/ioc/s01/src/main/java/com/imooc/spring/ioc/SpringApplication.java
+++ java_proj/imooc_java_engineer_course/spring/ioc/s01/src/main/java/com/imooc/spring/ioc/SpringApplication.java
@@ -1,6 +1,7 @@
 package com.imooc.spring.ioc;
 
 import com.imooc.spring.ioc.entity.Apple;
+import com.imooc.spring.ioc.entity.Child;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,5 +11,11 @@ public class SpringApplication {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         Apple sweetApple = context.getBean("sweetApple", Apple.class);
         System.out.println(sweetApple.getTitle());
+        Child lily = context.getBean("lily", Child.class);
+        lily.eat();
+        Child andy = context.getBean("andy", Child.class);
+        andy.eat();
+        Child luna = context.getBean("luna", Child.class);
+        luna.eat();
     }
 }
