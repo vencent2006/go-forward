f60da8a763dbe2d829c2d21a4feff67eb0f34a14
vencent2006 <lewis_sun@foxmail.com>
2024-05-22 09:35:50 +0800

    exercise 2024.05.21

---
+++ python_proj/python_2022/from0to1_live_video/01/example.py
@@ -0,0 +1,10 @@
+data1 = "124323454"
+data2 = '2'
+if data1 is data2:
+    print("data1和data2包含相同的元素")
+else:
+    print("data1和data2不包含相同的元素")
+if data1 is not data2:
+    print("data1和data2不包含相同的元素")
+else:
+    print("data1和data2包含相同的元素")
