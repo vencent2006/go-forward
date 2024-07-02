000f6476792692a94489899382cbe428e225730a
vencent2006 <lewis_sun@foxmail.com>
2024-07-02 14:44:40 +0800

    python python_2022 exercise db conn

--- python_proj/python_2022/week_06_03_zonghe_xiangmu_shizhan/gift/demo/exercise/exercise.py
+++ python_proj/python_2022/week_06_03_zonghe_xiangmu_shizhan/gift/demo/exercise/exercise.py
@@ -126,3 +126,26 @@ class ExerciseTestCase(unittest.TestCase):
         print(one_str.lower())
         print(one_str.capitalize())
         print(one_str.title())
+
+    def test_mysql_connect(self):
+        import pymysql
+
+        # 打开数据库连接
+        db = pymysql.connect(host='localhost',
+                             user='root',
+                             password='qwer1234',
+                             database='python_2022')
+
+        # 使用 cursor() 方法创建一个游标对象 cursor
+        cursor = db.cursor()
+
+        # 使用 execute()  方法执行 SQL 查询
+        cursor.execute("SELECT VERSION()")
+
+        # 使用 fetchone() 方法获取单条数据.
+        data = cursor.fetchone()
+
+        print("Database version : %s " % data)
+
+        # 关闭数据库连接
+        db.close()
