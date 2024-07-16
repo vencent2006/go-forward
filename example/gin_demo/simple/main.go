3ca4984f1f891cdb87f6b9c0944e0fb8a7a92fff
vencent2006 <lewis_sun@foxmail.com>
2024-07-16 19:56:48 +0800

    gin simple demo

--- example/gin_demo/simple/main.go
+++ example/gin_demo/simple/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Response struct {
+	Code    int                    `json:"code"`
+	Message string                 `json:"msg"`
+	Data    map[string]interface{} `json:"data"`
+}
+
 func main() {
-	$END$
+	r := gin.Default()
+
+	r.GET("/community/get", func(c *gin.Context) {
+		data := &Response{
+			Code:    10000,
+			Message: "succ",
+			Data: map[string]interface{}{
+				"community_id": "bobo123",
+			},
+		}
+		c.JSON(http.StatusOK, data)
+	})
+
+	r.Run(":8911")
 }
