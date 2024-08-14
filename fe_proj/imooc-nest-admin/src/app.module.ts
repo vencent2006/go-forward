fb685f66dd9f83d0d259799e22b8ad32f02467ad
vencent2006 <lewis_sun@foxmail.com>
2024-08-14 19:28:26 +0800

    1. install typeorm 2. connect mysql

--- fe_proj/imooc-nest-admin/src/app.module.ts
+++ fe_proj/imooc-nest-admin/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestService } from './test.service';
@@ -7,7 +8,18 @@ import { AuthModule } from './modules/auth/auth.module';
 import { BookModule } from './modules/book/book.module';
 
 @Module({
-  imports: [UserModule, AuthModule, BookModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'qwer1234',
+      database: 'vben-book-dev'
+    }),
+    UserModule,
+    AuthModule,
+    BookModule],
   controllers: [AppController],
   providers: [AppService, TestService],
 })
