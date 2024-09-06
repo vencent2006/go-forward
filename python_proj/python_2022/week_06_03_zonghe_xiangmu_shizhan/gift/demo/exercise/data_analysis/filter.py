b90472b16220ed306a77ecb447027041faf7b5f8
vencent2006 <lewis_sun@foxmail.com>
2024-09-06 10:25:05 +0800

    python filter by map

---
+++ python_proj/python_2022/week_06_03_zonghe_xiangmu_shizhan/gift/demo/exercise/data_analysis/filter.py
@@ -0,0 +1,55 @@
+payed_map = {}
+
+with open("payed.txt", "r") as file:
+    lines = file.readlines()
+    print('payed.txt 行数 =', len(lines))
+    for line in lines:
+        line = line.strip()  # 去除每行的换行符
+        # 进行需要的操作
+        payed_map[line] = 1
+        # print(line, end="\n")
+
+print('支付了的订单数 =', len(payed_map), end="\n\n")
+
+
+def filterPayed(filter_map, list_filename):
+    local_map = {}
+    local_set = set()
+    with open(list_filename, "r") as file:
+        lines = file.readlines()
+        print(f'{list_filename} 行数 =', len(lines))
+        for line in lines:
+            line = line.strip()  # 去除每行的换行符
+            # 进行需要的操作
+            local_set.add(line)
+            if line in filter_map:
+                local_map[line] = 1
+    print(f'{list_filename} 去重 =', len(local_set))
+    print(f'{list_filename} 匹配 =', len(local_map), end="\n\n")
+
+
+filterPayed(payed_map, "xiquezhuangban.txt")
+filterPayed(payed_map, "fulishang.txt")
+
+# xiquezhuangban_payed_map = {}
+# with open("xiquezhuangban.txt", "r") as file:
+#     lines = file.readlines()
+#     print('xiquezhuangban.txt lines =', len(lines))
+#     for line in lines:
+#         line = line.strip()  # 去除每行的换行符
+#         # 进行需要的操作
+#         if line in payed_map:
+#             xiquezhuangban_payed_map[line] = 1
+#
+# print('len(xiquezhuangban_payed_map = ', len(xiquezhuangban_payed_map))
+#
+# fulishang_payed_map = {}
+# with open("fulishang.txt", "r") as file:
+#     lines = file.readlines()
+#     print('fulishang.txt lines =', len(lines))
+#     for line in lines:
+#         line = line.strip()  # 去除每行的换行符
+#         # 进行需要的操作
+#         if line in payed_map:
+#             fulishang_payed_map[line] = 1
+# print('len(fulishang_payed_map = ', len(fulishang_payed_map))
