9aefb920290bd7935ab49df6d5a7d20977b4cca1
vencent2006 <lewis_sun@foxmail.com>
2024-12-03 10:35:45 +0800

    ts-learn2 init

---
+++ fe_proj/ts_demo/ts-learn2/index.ts
@@ -0,0 +1,9 @@
+const msg:string = 'hello ts!!'
+console.log(msg)
+
+
+// array
+const arr4:(string|number)[]=['2323', 2323]
+console.log(arr4)
+const arr5:Array<string|number> = ['2323', 2323]
+console.log(arr5)
