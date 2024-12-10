3bf4ee0eb141d911cecb7e1c336cd9ed4ac68dc0
vencent2006 <lewis_sun@foxmail.com>
2024-12-10 20:32:55 +0800

    nestjs miji ch04

---
+++ fe_proj/nestjs_miji/ch04/xxx/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public', { prefix: '/static' });
+  await app.listen(3000);
+}
+bootstrap();
---
+++ fe_proj/nestjs_miji/ch04/xxx/src/person/person.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Post()
+  create(@Body() createPersonDto: CreatePersonDto) {
+    return `receive: ${JSON.stringify(createPersonDto)}`;
+    // return this.personService.create(createPersonDto);
+  }
+  // @UseInterceptors(AnyFilesInterceptor())
+  @Post('file')
+  body2(
+    @Body() createPersonDto: CreatePersonDto,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    return `receive: ${JSON.stringify(createPersonDto)}`;
+  }
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.personService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
