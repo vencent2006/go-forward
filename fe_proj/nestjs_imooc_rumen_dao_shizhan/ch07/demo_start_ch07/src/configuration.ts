c6d89a693133bf0547a14c220fd1a2772a000fe0
vencent2006 <lewis_sun@foxmail.com>
2024-12-14 10:58:34 +0800

    imooc nestjs cong rumen dao shizhan ch07-06

--- fe_proj/nestjs_imooc_rumen_dao_shizhan/ch07/demo_start_ch07/src/configuration.ts
+++ fe_proj/nestjs_imooc_rumen_dao_shizhan/ch07/demo_start_ch07/src/configuration.ts
@@ -1,11 +1,22 @@
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
+import * as _ from 'lodash';
 
-const YAML_CONFIG_FILENAME = 'config.yaml';
-const filePath = join(__dirname, '../config/', YAML_CONFIG_FILENAME);
+const YAML_COMMON_CONFIG_FILENAME = 'config.yaml';
+const filePath = join(__dirname, '../config', YAML_COMMON_CONFIG_FILENAME);
+
+const envPath = join(
+  __dirname,
+  '../config',
+  `config.${process.env.NODE_ENV || 'development'}.yaml`,
+);
+
+const commonConfig = yaml.load(readFileSync(filePath, 'utf8'));
+const envConfig = yaml.load(readFileSync(envPath, 'utf8'));
 
 // 因为ConfigModule有一个load方法，它需要的是一个函数
 export default () => {
-  return yaml.load(readFileSync(filePath, 'utf8'));
+  // 配置文件merge
+  return _.merge(commonConfig, envConfig);
 };
