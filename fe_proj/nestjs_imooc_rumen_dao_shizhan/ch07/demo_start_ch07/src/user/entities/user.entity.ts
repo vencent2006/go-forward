c5238f9a47fe04b5f66a7aa9c41c54e5da4e2fb9
vencent2006 <lewis_sun@foxmail.com>
2024-12-15 16:49:36 +0800

    imooc nestjs cong rumen dao shizhan ch08-07

--- fe_proj/nestjs_imooc_rumen_dao_shizhan/ch07/demo_start_ch07/src/user/entities/user.entity.ts
+++ fe_proj/nestjs_imooc_rumen_dao_shizhan/ch07/demo_start_ch07/src/user/entities/user.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Logs } from "src/logs/logs.entity";
+import { Roles } from "src/roles/roles.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn, ManyToMany, JoinTable } from "typeorm";
 
 @Entity()
 export class User {
@@ -10,4 +12,12 @@ export class User {
 
   @Column()
   password: string;
+
+  // typescript 与 数据库的关联关系 mapping
+  @OneToMany(()=>Logs, (logs)=>logs.user)
+  logs: Logs[];
+
+  @ManyToMany(()=>Roles, (roles)=>roles.users)
+  @JoinTable({name: 'users_roles'})
+  roles: Roles[];
 }
