43a45617c7903d35a523a04b51df3ae652b93c40
vencent2006 <lewis_sun@foxmail.com>
2024-12-18 18:55:51 +0800

    5.5 增加AOP打印请求参数和返回结果

--- java_proj/imooc-nls/business/src/main/java/com/jiawa/nls/business/aspect/LogAspect.java
+++ java_proj/imooc-nls/business/src/main/java/com/jiawa/nls/business/aspect/LogAspect.java
@@ -1,10 +1,19 @@
 package com.jiawa.nls.business.aspect;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Aspect
@@ -31,21 +40,54 @@ public class LogAspect {
     // 前置通知
     @Before("pointcut()")
     public void doBefore(JoinPoint joinPoint){
-        log.info("前置通知");
+        // log.info("前置通知");
     }
 
     // 环绕通知
     @Around("pointcut()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        log.info("环绕通知开始");
+        log.info("------------ 环绕通知开始 ------------");
+        long startTime = System.currentTimeMillis();
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = proceedingJoinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        log.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        log.info("类名方法: {} {}", signature.getDeclaringTypeName(), name);
+        log.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        // 排除特殊类型的参数，如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                || args[i] instanceof ServletResponse
+                || args[i] instanceof MultipartFile){
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {"cvv2", "idCard"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+        excludeFilter.addExcludes(excludeProperties);
+        log.info("请求参数: {}", JSONObject.toJSONString(arguments, excludeFilter));
         Object result = proceedingJoinPoint.proceed();
-        log.info("环绕通知结束");
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        log.info("返回结果: {}", JSONObject.toJSONString(result, excludeFilter));
+        log.info("------------ 环绕通知结束 耗时: {} ms ------------", System.currentTimeMillis() - startTime);
         return result;
     }
 
     // 后置通知
     @After("pointcut()")
     public void doAfter(JoinPoint joinPoint){
-        log.info("后置通知");
+        // log.info("后置通知");
     }
 }
