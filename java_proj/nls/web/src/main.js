c7bacddaaec5094673db10a9b266f3c4b8774f75
vencent2006 <lewis_sun@foxmail.com>
2024-12-28 00:14:40 +0800

    6.2 web模块集成 Ant Design Vue，npm install --save ant-design-vue@4.1.0

--- java_proj/nls/web/src/main.js
+++ java_proj/nls/web/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
