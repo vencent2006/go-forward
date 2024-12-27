7edd3d67fe35ca464e275cf8af8ae74a78b0efce
vencent2006 <lewis_sun@foxmail.com>
2024-12-27 14:55:49 +0800

    5.12 封装请求参数，让前端可以统一处理

--- java_proj/nls/business/src/main/java/com/jiawa/nls/business/controller/TestController.java
+++ java_proj/nls/business/src/main/java/com/jiawa/nls/business/controller/TestController.java
@@ -2,6 +2,7 @@ package com.jiawa.nls.business.controller;
 
 import com.jiawa.nls.business.domain.Demo;
 import com.jiawa.nls.business.req.DemoQueryReq;
+import com.jiawa.nls.business.resp.CommonResp;
 import com.jiawa.nls.business.service.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +16,23 @@ public class TestController {
     private DemoService demoService;
 
     @GetMapping("/hello1")
-    public String hello(){
-        return "Hello world!";
+    public CommonResp<String> hello() {
+        // return "Hello world!";
+        return new CommonResp<>("Hello world!");
     }
 
     @GetMapping("/count")
-    public int count(){
-        return demoService.count();
+    public CommonResp<Integer> count() {
+        // return demoService.count();
+        return new CommonResp<>(demoService.count());
     }
+
     @GetMapping("/query")
-    public List<Demo> query(DemoQueryReq req){
-        return demoService.query(req);
+    public CommonResp<List<Demo>> query(DemoQueryReq req) {
+        List<Demo> demoList = demoService.query(req);
+        // CommonResp<List<Demo>> listCommonResp = new CommonResp<>();
+        // listCommonResp.setContent(demoList);
+        // return listCommonResp;
+        return new CommonResp<>(demoList);
     }
 }
