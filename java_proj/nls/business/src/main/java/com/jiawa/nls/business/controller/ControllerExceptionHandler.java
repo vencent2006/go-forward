58fa8f7eb1cfe3a3cb4335ce9184440097918659
vencent2006 <lewis_sun@foxmail.com>
2024-12-27 16:09:16 +0800

    5.12 增加统一的异常处理，避免将堆栈异常直接返回给前端

---
+++ java_proj/nls/business/src/main/java/com/jiawa/nls/business/controller/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.jiawa.nls.business.controller;
+
+import com.jiawa.nls.business.resp.CommonResp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理，数据预处理等
+ */
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    /**
+     * 所有异常统一处理
+     *
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = Exception.class) // 处理是什么异常
+    @ResponseBody
+    public CommonResp<Object> exceptionHandler(Exception e) {
+        CommonResp<Object> commonResp = new CommonResp<>();
+        log.error("系统异常: ", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常, 请联系管理员");
+        return commonResp;
+    }
+}
