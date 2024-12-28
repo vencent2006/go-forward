6214b3bdcf13b9509db62a2f071bf5848a069459
vencent2006 <lewis_sun@foxmail.com>
2024-12-28 17:04:39 +0800

    6.6 vue组件开发，增加the-sider组件

--- java_proj/nls/web/src/view/home.vue
+++ java_proj/nls/web/src/view/home.vue
@@ -2,51 +2,7 @@
     <a-layout>
         <the-header></the-header>
         <a-layout>
-            <a-layout-sider width="200" style="background: #fff">
-                <a-menu
-                        v-model:selectedKeys="selectedKeys2"
-                        v-model:openKeys="openKeys"
-                        mode="inline"
-                        :style="{ height: '100%', borderRight: 0 }"
-                >
-                    <a-sub-menu key="sub1">
-                        <template #title>
-              <span>
-                <user-outlined/>
-                subnav 1
-              </span>
-                        </template>
-                        <a-menu-item key="1">option1</a-menu-item>
-                        <a-menu-item key="2">option2</a-menu-item>
-                        <a-menu-item key="3">option3</a-menu-item>
-                        <a-menu-item key="4">option4</a-menu-item>
-                    </a-sub-menu>
-                    <a-sub-menu key="sub2">
-                        <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-                        </template>
-                        <a-menu-item key="5">option5</a-menu-item>
-                        <a-menu-item key="6">option6</a-menu-item>
-                        <a-menu-item key="7">option7</a-menu-item>
-                        <a-menu-item key="8">option8</a-menu-item>
-                    </a-sub-menu>
-                    <a-sub-menu key="sub3">
-                        <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-                        </template>
-                        <a-menu-item key="9">option9</a-menu-item>
-                        <a-menu-item key="10">option10</a-menu-item>
-                        <a-menu-item key="11">option11</a-menu-item>
-                        <a-menu-item key="12">option12</a-menu-item>
-                    </a-sub-menu>
-                </a-menu>
-            </a-layout-sider>
+            <the-sider></the-sider>
             <a-layout style="padding: 0 24px 24px">
                 <a-breadcrumb style="margin: 16px 0">
                     <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -63,11 +19,8 @@
     </a-layout>
 </template>
 <script setup>
-import {ref} from 'vue';
 import TheHeader from "../components/the-header.vue";
-
-const selectedKeys2 = ref(['1']);
-const openKeys = ref(['sub1']);
+import TheSider from "../components/the-sider.vue";
 </script>
 <style scoped>
 .site-layout-background {
