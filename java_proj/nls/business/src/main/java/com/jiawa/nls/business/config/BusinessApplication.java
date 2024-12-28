f75d1f5bf636aeb2aa21b65cab01539bbe6be62c
vencent2006 <lewis_sun@foxmail.com>
2024-12-29 00:14:41 +0800

    6.7 后端增加配置，允许跨域

--- java_proj/nls/business/src/main/java/com/jiawa/nls/business/config/BusinessApplication.java
+++ java_proj/nls/business/src/main/java/com/jiawa/nls/business/config/BusinessApplication.java
@@ -1,10 +1,12 @@
-package com.jiawa.nls.business;
+package com.jiawa.nls.business.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
+@ComponentScan("com.jiawa") // 确定扫描范围
 @MapperScan("com.jiawa.nls.business.mapper") // 确定mapper的位置
 @SpringBootApplication
 public class BusinessApplication {
