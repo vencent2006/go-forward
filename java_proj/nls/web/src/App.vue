94cf2d11c16bef9ab091b7823a1d34cec9f76de5
vencent2006 <lewis_sun@foxmail.com>
2024-12-28 15:50:10 +0800

    6.4 增加官网示例布局，在官网源码的基础上删除：#components-layout-demo-top-side-2

--- java_proj/nls/web/src/App.vue
+++ java_proj/nls/web/src/App.vue
@@ -1,39 +1,114 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-        </a>
-    </div>
-
-    <a-button type="dashed">
-        <LeftCircleTwoTone/>
-        Dashed Button
-    </a-button>
-    <StepForwardOutlined/>
-    <LeftCircleTwoTone/>
-    <HelloWorld msg="Vite + Vue"/>
+    <a-layout>
+        <a-layout-header class="header">
+            <div class="logo"/>
+            <a-menu
+                    theme="dark"
+                    mode="horizontal"
+                    v-model:selectedKeys="selectedKeys1"
+                    :style="{ lineHeight: '64px' }"
+            >
+                <a-menu-item key="1">nav 1</a-menu-item>
+                <a-menu-item key="2">nav 2</a-menu-item>
+                <a-menu-item key="3">nav 3</a-menu-item>
+            </a-menu>
+        </a-layout-header>
+        <a-layout>
+            <a-layout-sider width="200" style="background: #fff">
+                <a-menu
+                        mode="inline"
+                        v-model:selectedKeys="selectedKeys2"
+                        v-model:openKeys="openKeys"
+                        :style="{ height: '100%', borderRight: 0 }"
+                >
+                    <a-sub-menu key="sub1">
+                        <template #title>
+              <span>
+                <user-outlined/>
+                subnav 1
+              </span>
+                        </template>
+                        <a-menu-item key="1">option1</a-menu-item>
+                        <a-menu-item key="2">option2</a-menu-item>
+                        <a-menu-item key="3">option3</a-menu-item>
+                        <a-menu-item key="4">option4</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub2">
+                        <template #title>
+              <span>
+                <laptop-outlined/>
+                subnav 2
+              </span>
+                        </template>
+                        <a-menu-item key="5">option5</a-menu-item>
+                        <a-menu-item key="6">option6</a-menu-item>
+                        <a-menu-item key="7">option7</a-menu-item>
+                        <a-menu-item key="8">option8</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub3">
+                        <template #title>
+              <span>
+                <notification-outlined/>
+                subnav 3
+              </span>
+                        </template>
+                        <a-menu-item key="9">option9</a-menu-item>
+                        <a-menu-item key="10">option10</a-menu-item>
+                        <a-menu-item key="11">option11</a-menu-item>
+                        <a-menu-item key="12">option12</a-menu-item>
+                    </a-sub-menu>
+                </a-menu>
+            </a-layout-sider>
+            <a-layout style="padding: 0 24px 24px">
+                <a-breadcrumb style="margin: 16px 0">
+                    <a-breadcrumb-item>Home</a-breadcrumb-item>
+                    <a-breadcrumb-item>List</a-breadcrumb-item>
+                    <a-breadcrumb-item>App</a-breadcrumb-item>
+                </a-breadcrumb>
+                <a-layout-content
+                        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+                >
+                    Content
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
+    </a-layout>
 </template>
+<script>
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons-vue';
+import {defineComponent, ref} from 'vue';
 
-<style scoped>
+export default defineComponent({
+    components: {
+        UserOutlined,
+        LaptopOutlined,
+        NotificationOutlined,
+    },
+    setup() {
+        return {
+            selectedKeys1: ref(['2']),
+            selectedKeys2: ref(['1']),
+            collapsed: ref(false),
+            openKeys: ref(['sub1']),
+        };
+    },
+});
+</script>
+<style>
 .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+    float: left;
+    width: 120px;
+    height: 31px;
+    margin: 16px 24px 16px 0;
+    background: rgba(255, 255, 255, 0.3);
 }
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+.ant-row-rtl .logo {
+    float: right;
+    margin: 16px 0 16px 24px;
 }
 
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+.site-layout-background {
+    background: #fff;
 }
 </style>
