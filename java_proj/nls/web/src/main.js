843553123da299aea2e47ed72684d792ed5071c9
vencent2006 <lewis_sun@foxmail.com>
2024-12-28 16:21:13 +0800

    6.5 集成vue-router, 增加/home路由 npm install vue-router

--- java_proj/nls/web/src/main.js
+++ java_proj/nls/web/src/main.js
@@ -5,9 +5,13 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 // 将antDesign的图标都引进
 import * as Icons from '@ant-design/icons-vue';
+import router from "./router";
 
 const app = createApp(App);
-app.use(Antd).mount('#app');
+app
+    .use(Antd) // ant design vue
+    .use(router) // vue-router
+    .mount('#app');
 
 // 全局使用图标
 const icons = Icons;
