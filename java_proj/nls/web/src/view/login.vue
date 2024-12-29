2c904248d02b1df40ee46bdf9ada3e1bd1c18785
vencent2006 <lewis_sun@foxmail.com>
2024-12-30 00:14:38 +0800

    7.2 制作登录页面

--- java_proj/nls/web/src/view/login.vue
+++ java_proj/nls/web/src/view/login.vue
@@ -1,19 +1,23 @@
 <template>
     <a-form
-            :model="formState"
+            :model="loginMember"
             name="basic"
             :label-col="{ span: 8 }"
             :wrapper-col="{ span: 16 }"
             autocomplete="off"
-            @finish="onFinish"
+            @finish="login"
             @finishFailed="onFinishFailed"
     >
         <a-form-item
-                label="Username"
+                label="mobile"
                 name="username"
-                :rules="[{ required: true, message: 'Please input your username!' }]"
+                :rules="[{ required: true, message: '请输入手机号' }]"
         >
-            <a-input v-model:value="formState.username"/>
+            <a-input v-model:value="loginMember.mobile">
+                <template #prefix>
+                    <MobileOutlined style="margin-left: 15px"/>
+                </template>
+            </a-input>
         </a-form-item>
 
         <a-form-item
@@ -21,28 +25,27 @@
                 name="password"
                 :rules="[{ required: true, message: 'Please input your password!' }]"
         >
-            <a-input-password v-model:value="formState.password"/>
-        </a-form-item>
-
-        <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-            <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+            <a-input-password v-model:value="loginMember.password">
+                <template #prefix>
+                    <LockOutlined style="margin-left: 15px"/>
+                </template>
+            </a-input-password>
         </a-form-item>
 
         <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-            <a-button type="primary" html-type="submit">Submit</a-button>
+            <a-button type="primary" block html-type="submit">登&nbsp;录</a-button>
         </a-form-item>
     </a-form>
 </template>
 <script setup>
-import {reactive} from 'vue';
+import {ref} from 'vue';
 
-const formState = reactive({
-    username: '',
+const loginMember = ref({
+    mobile: '',
     password: '',
-    remember: true,
 });
-const onFinish = values => {
-    console.log('Success:', values);
+const login = values => {
+    console.log('开始登录:', values);
 };
 const onFinishFailed = errorInfo => {
     console.log('Failed:', errorInfo);
