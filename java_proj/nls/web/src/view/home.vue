180f4089ca6a33517054294ff9bbf2e690cf99cf
vencent2006 <lewis_sun@foxmail.com>
2024-12-29 17:13:26 +0800

    6.10 演示Vue3的响应式变量的用法

--- java_proj/nls/web/src/view/home.vue
+++ java_proj/nls/web/src/view/home.vue
@@ -12,7 +12,8 @@
                 <a-layout-content
                         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
                 >
-                    Content
+                    {{ resp }}
+                    <a-input v-model:value="resp" @click="onChange"/>
                 </a-layout-content>
             </a-layout>
         </a-layout>
@@ -22,6 +23,10 @@
 import TheHeader from "../components/the-header.vue";
 import TheSider from "../components/the-sider.vue";
 import axios from "axios";
+import {ref} from "vue";
+import {message} from "ant-design-vue";
+
+const resp = ref()
 
 axios.get("http://localhost:8080/nls/query", {
     params: {
@@ -29,7 +34,17 @@ axios.get("http://localhost:8080/nls/query", {
     }
 }).then(response => {
     // console.log(response);
+    let data = response.data
+    if (data.success) {
+        resp.value = data.content
+    } else {
+        message.error(data.message)
+    }
 })
+
+const onChange = () => {
+    console.log(resp.value)
+}
 </script>
 <style scoped>
 .site-layout-background {
