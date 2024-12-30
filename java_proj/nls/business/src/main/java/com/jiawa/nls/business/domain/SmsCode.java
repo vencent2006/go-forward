150829471c2746bc7edcb3c33bf3ade3614f0c8e
vencent2006 <lewis_sun@foxmail.com>
2024-12-30 17:25:10 +0800

    7.5 生成短信验证码表持久层代码

---
+++ java_proj/nls/business/src/main/java/com/jiawa/nls/business/domain/SmsCode.java
@@ -0,0 +1,92 @@
+package com.jiawa.nls.business.domain;
+
+import java.util.Date;
+
+public class SmsCode {
+    private Long id;
+
+    private String mobile;
+
+    private String code;
+
+    private String used;
+
+    private String status;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUsed() {
+        return used;
+    }
+
+    public void setUsed(String used) {
+        this.used = used;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", mobile=").append(mobile);
+        sb.append(", code=").append(code);
+        sb.append(", used=").append(used);
+        sb.append(", status=").append(status);
+        sb.append(", createAt=").append(createAt);
+        sb.append(", updateAt=").append(updateAt);
+        sb.append("]");
+        return sb.toString();
+    }
+}
