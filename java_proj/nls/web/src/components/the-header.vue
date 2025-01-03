8aa51eec1b96b5a00975cafce5dcb33f7c510096
vencent2006 <lewis_sun@foxmail.com>
2025-01-04 00:12:31 +0800

    9.4 修改顶部菜单和侧边栏菜单，实现页面跳转

--- java_proj/nls/web/src/components/the-header.vue
+++ java_proj/nls/web/src/components/the-header.vue
@@ -8,14 +8,23 @@
             </router-link>
         </div>
         <a-menu
-                v-model:selectedKeys="selectedKeys1"
+                v-model:selectedKeys="selectedKeys"
                 theme="dark"
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
         >
-            <a-menu-item key="1">nav 1111</a-menu-item>
-            <a-menu-item key="2">nav 2</a-menu-item>
-            <a-menu-item key="3">nav 3</a-menu-item>
+            <a-menu-item key="/home/welcome">
+                <router-link to="/home/welcome">
+                    <CoffeeOutlined/>
+                    <span>欢迎使用</span>
+                </router-link>
+            </a-menu-item>
+            <a-menu-item key="/home/help">
+                <router-link to="/home/help">
+                    <QuestionCircleOutlined/>
+                    <span>帮助文档</span>
+                </router-link>
+            </a-menu-item>
         </a-menu>
     </a-layout-header>
 </template>
@@ -23,7 +32,7 @@
 import {ref} from 'vue';
 import {useMemberStore} from "../stores/member.js";
 
-const selectedKeys1 = ref(['2']);
+const selectedKeys = ref(['/home/welcome']);
 const memberStore = useMemberStore()
 const member = memberStore.member
 </script>
