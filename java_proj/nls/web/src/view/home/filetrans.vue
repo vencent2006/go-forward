0f543b4173f3330f44167cf61433df7039c1b562
vencent2006 <lewis_sun@foxmail.com>
2025-01-04 17:43:32 +0800

    9.6 语音识别页面增加对话框，用于上传音频

--- java_proj/nls/web/src/view/home/filetrans.vue
+++ java_proj/nls/web/src/view/home/filetrans.vue
@@ -1,7 +1,22 @@
 <template>
-    <h1>语音识别</h1>
+    <div>
+        <a-button type="primary" @click="showModal">语音识别</a-button>
+        <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </a-modal>
+    </div>
 </template>
 <script setup>
+import {ref} from 'vue';
+
+const open = ref(false);
+const showModal = () => {
+    open.value = true;
+};
+const handleOk = e => {
+    console.log(e);
+    open.value = false;
+};
 </script>
-<style scoped>
-</style>
