c6a661607438c6f68295276654877d92b2cf1fb0
vencent2006 <lewis_sun@foxmail.com>
2025-01-04 20:14:40 +0800

    9.7 限制上传文件的大小

--- java_proj/nls/web/src/view/home/filetrans-upload.vue
+++ java_proj/nls/web/src/view/home/filetrans-upload.vue
@@ -3,7 +3,12 @@
         <a-button type="primary" @click="selectFile" size="large">
             <span><UploadOutlined/>选择音频</span>
         </a-button>
-        <input type="file" accept=".mp3,.wav,.m4a" style="display: none" ref="fileUploadCom"/>
+        <input type="file"
+               accept=".mp3,.wav,.m4a"
+               style="display: none"
+               ref="fileUploadCom"
+               @change="uploadFile"
+        />
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
@@ -11,6 +16,7 @@
 </template>
 <script setup>
 import {ref} from 'vue';
+import {notification} from "ant-design-vue";
 
 const open = ref(false);
 const showModal = () => {
@@ -21,10 +27,29 @@ const handleOk = e => {
     open.value = false;
 };
 
-// 选择文件
+// ----------- 选择文件 -----------
 const fileUploadCom = ref();
 const selectFile = () => {
+    fileUploadCom.value.value = '';// 清空input
     fileUploadCom.value.click();
+}
+
+// ----------- 上传文件 -----------
+const uploadFile = () => {
+    // message.info(fileUploadCom.value.files[0].name)
+    const file = fileUploadCom.value.files[0];
+    console.log(file)
+
+    // 音频文件最大为500M
+    const max = 500 * 1024 * 1024;
+    const size = file.size;
+    if (size > max) {
+        // message.error('文件大小不能超过500M')
+        notification['warning']({
+            message: '系统提示',
+            description: '文件大小超过最大限制，最大为500M',
+        })
+    }
 }
 
 // 使用 defineExpose 向外暴露指定的数据和方法
