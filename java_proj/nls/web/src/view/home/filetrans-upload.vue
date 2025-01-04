cc90776e0d6f3d0525859052ca291c47b1fe7cb1
vencent2006 <lewis_sun@foxmail.com>
2025-01-04 19:39:52 +0800

    9.7 选择增加音频按钮，点击可以选择文件

--- java_proj/nls/web/src/view/home/filetrans-upload.vue
+++ java_proj/nls/web/src/view/home/filetrans-upload.vue
@@ -1,5 +1,9 @@
 <template>
     <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+        <a-button type="primary" @click="selectFile" size="large">
+            <span><UploadOutlined/>选择音频</span>
+        </a-button>
+        <input type="file" accept="audio/*" style="display: none" ref="fileUploadCom"/>
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
@@ -16,6 +20,12 @@ const handleOk = e => {
     console.log(e);
     open.value = false;
 };
+
+// 选择文件
+const fileUploadCom = ref();
+const selectFile = () => {
+    fileUploadCom.value.click();
+}
 
 // 使用 defineExpose 向外暴露指定的数据和方法
 defineExpose({
