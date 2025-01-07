f0afdc7e7c268164fdddc2fa85938b0671fd91c0
vencent2006 <lewis_sun@foxmail.com>
2025-01-07 08:09:40 +0800

    011 mybatis优化器动态加载

--- java_proj/ape-frame/ape-common/ape-common-mybatisplus/src/main/java/com/jingdianjichi/config/MybatisConfiguration.java
+++ java_proj/ape-frame/ape-common/ape-common-mybatisplus/src/main/java/com/jingdianjichi/config/MybatisConfiguration.java
@@ -1,12 +1,15 @@
 package com.jingdianjichi.config;
 
 import com.jingdianjichi.inteceptor.SqlBeautyInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisConfiguration {
     @Bean
+    @ConditionalOnProperty(name = "sql.beauty.show", havingValue = "true", matchIfMissing = true)
+    // 只有配置了sql.beauty.show=true才会生效
     public SqlBeautyInterceptor sqlBeautyInterceptor() {
         return new SqlBeautyInterceptor();
     }
