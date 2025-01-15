8af979c3434c44a82d9a794a4cae56cbc6cc1b25
vencent2006 <lewis_sun@foxmail.com>
2025-01-15 15:16:41 +0800

    021 RedisTemplate集成

---
+++ java_proj/ape-frame/ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.jingdianjichi.redis.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+
+    /**
+     * 自定义序列化
+     *
+     * @return
+     */
+    private Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer() {
+        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);// 序列化所有的字段
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);// 忽略json字符串中不识别的属性
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jsonRedisSerializer.setObjectMapper(objectMapper);
+        return jsonRedisSerializer;
+    }
+}
--- java_proj/ape-frame/ape-user/src/main/java/com/jingdianjichi/user/controller/TestController.java
+++ java_proj/ape-frame/ape-user/src/main/java/com/jingdianjichi/user/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.jingdianjichi.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("/testRedis")
+    public void testRedis() {
+        redisTemplate.opsForValue().set("name", "鸡翅");
     }
 }
