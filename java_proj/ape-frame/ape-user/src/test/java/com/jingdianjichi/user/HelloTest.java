eacf9724bd5679d8ca4df63ec001c6bf834667cd
vencent2006 <lewis_sun@foxmail.com>
2025-01-16 09:59:21 +0800

    测试test，有一个不好使

---
+++ java_proj/ape-frame/ape-user/src/test/java/com/jingdianjichi/user/HelloTest.java
@@ -0,0 +1,10 @@
+package com.jingdianjichi.user;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    @Test
+    public void hello() {
+        System.out.println("hello");
+    }
+}
