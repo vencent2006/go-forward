338dde928e9fd1edd4b4df2d468fb946a5dc53f7
vencent2006 <lewis_sun@foxmail.com>
2025-02-19 00:21:26 +0800

    router init

--- fe_proj/consult-patient-vue3/src/router/index.ts
+++ fe_proj/consult-patient-vue3/src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// createWebHashHistory 哈希路由 会有 # 号
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // 路由模式
   routes: [],
 })
 
