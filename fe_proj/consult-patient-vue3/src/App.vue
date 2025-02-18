1628a397cbf123f287da90665e35a6a5e1240ce6
vencent2006 <lewis_sun@foxmail.com>
2025-02-18 23:47:23 +0800

    code adjust

--- fe_proj/consult-patient-vue3/src/App.vue
+++ fe_proj/consult-patient-vue3/src/App.vue
@@ -1,24 +1,4 @@
-<script setup lang="ts">
-import { reactive, ref } from 'vue'
-
-type Item = {
-  id: string
-  name: string
-  price: number
-}
-const list = ref<Item[]>([])
-list.value = [
-  { id: '1', name: 'item 1', price: 100 },
-  { id: '2', name: 'item 2', price: 200 },
-  { id: '3', name: 'item 3', price: 300 },
-]
-const form = reactive({
-  username: '',
-  password: '',
-})
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <li v-for="item in list" :key="item.id">{{ item.name }}</li>
-  </div>
+  <div>App</div>
 </template>
