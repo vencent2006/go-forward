4c69b954fd4c7ac189c51ae56ddd790dadeb050e
vencent2006 <lewis_sun@foxmail.com>
2025-02-20 00:04:57 +0800

    feat: 用户仓库和持久化

--- fe_proj/consult-patient-vue3/src/App.vue
+++ fe_proj/consult-patient-vue3/src/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
 import { Button as VantButton } from 'vant'
+import { useUserStore } from './stores/user'
+const store = useUserStore()
 </script>
 <template>
-  <vant-button type="primary">按钮</vant-button>
+  <div>
+    App {{ store.user }}
+    <vant-button
+      type="primary"
+      @click="
+        store.setUser({
+          id: '1',
+          avatar: '1',
+          token: '1',
+          mobile: '1',
+          account: '1',
+        })
+      "
+      >登录</vant-button
+    >
+    <vant-button type="primary" @click="store.delUser()">退出</vant-button>
+  </div>
 </template>
--- fe_proj/consult-patient-vue3/src/main.ts
+++ fe_proj/consult-patient-vue3/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import persist from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +11,7 @@ import './styles/main.scss'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(persist))
 app.use(router)
 
 app.mount('#app')
