14f5c8d560fcce8707a8a3734fb7132489319d59
vencent2006 <lewis_sun@foxmail.com>
2025-02-21 20:12:44 +0800

    feat: day5-5 cp-nav-bar 回退功能

--- fe_proj/consult-patient-vue3/src/App.vue
+++ fe_proj/consult-patient-vue3/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div class="app">
+    <router-link to="/login">登录</router-link>
     <!-- 路由出口 一级路由 -->
     <router-view></router-view>
   </div>
--- fe_proj/consult-patient-vue3/src/components/CpNavBar.vue
+++ fe_proj/consult-patient-vue3/src/components/CpNavBar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
 // 1. 通过props实现标题和右侧文字的设置
 defineProps<{
   title?: string
@@ -11,12 +13,22 @@ const emit = defineEmits<{
 const onClickRight = () => {
   emit('click-right')
 }
+// 3. 回退 了解 history.state 信息，监听箭头的点击事件按条件进行跳转
+const router = useRouter()
+const onClickLeft = () => {
+  if (history.state?.back) {
+    router.back()
+  } else {
+    router.push('/')
+  }
+}
 </script>
 <template>
   <!-- 固定定位 左侧箭头 标题 右侧文字 -->
   <van-nav-bar
     fixed
     left-arrow
+    @click-left="onClickLeft"
     :title="title"
     :right-text="rightText"
     @click-right="onClickRight"
