4b4dbea1a9849848902e9d8e343dcaf5f4025c63
vencent2006 <lewis_sun@foxmail.com>
2025-02-21 23:19:14 +0800

    feat: day5-7 登录页面布局和剔除多余代码

--- fe_proj/consult-patient-vue3/src/App.vue
+++ fe_proj/consult-patient-vue3/src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="app">
-    <router-link to="/login">登录</router-link>
-    <!-- 路由出口 一级路由 -->
-    <router-view></router-view>
-  </div>
+  <!-- 路由出口 一级路由 -->
+  <router-view></router-view>
 </template>
 
 <style lang="scss" scoped></style>
--- fe_proj/consult-patient-vue3/src/views/Login/index.vue
+++ fe_proj/consult-patient-vue3/src/views/Login/index.vue
@@ -6,10 +6,99 @@ const fn = () => {
 
 <template>
   <div class="login-page">
-    <cp-nav-bar @click-right="fn" title="登录" right-text="注册"></cp-nav-bar>
-    <van-button>van-button</van-button>
-    <van-checkbox>van-checkbox</van-checkbox>
+    <cp-nav-bar right-text="注册" @click-right="$router.push('/register')"></cp-nav-bar>
+    <!-- 头部 -->
+    <div class="login-head">
+      <h3>密码登录</h3>
+      <a href="javascript:;">
+        <span> 短信验证码登录 </span>
+        <van-icon name="arrow"></van-icon>
+      </a>
+    </div>
+    <!-- 表单 -->
+    <van-form autocomplete="off">
+      <van-field placeholder="请输入手机号" type="tel"></van-field>
+      <van-field placeholder="请输入密码" type="password"></van-field>
+
+      <div class="cp-cell">
+        <van-checkbox>
+          <span>我已同意</span>
+          <a href="javascript:;">用户协议</a>
+          <span>及</span>
+          <a href="javascript:;">隐私条款</a>
+        </van-checkbox>
+      </div>
+      <div class="cp-cell">
+        <van-button native-type="submit" block round type="primary"> 登 录 </van-button>
+      </div>
+      <div class="cp-cell">
+        <a href="javascript:;">忘记密码？</a>
+      </div>
+    </van-form>
+    <!-- 底部 -->
+    <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <a class="icon">
+        <img src="@/assets/qq.svg" alt="" />
+      </a>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  &-page {
+    padding-top: 46px;
+  }
+  &-head {
+    display: flex;
+    padding: 30px 30px 50px;
+    justify-content: space-between;
+    align-items: flex-end;
+    line-height: 1;
+    h3 {
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+    }
+  }
+  &-other {
+    margin-top: 60px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+.van-form {
+  padding: 0 14px;
+  .cp-cell {
+    height: 52px;
+    line-height: 24px;
+    padding: 14px 16px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .van-checkbox {
+      a {
+        color: var(--cp-primary);
+        padding: 0 5px;
+      }
+    }
+  }
+  .btn-send {
+    color: var(--cp-primary);
+    &.active {
+      color: rgba(22, 194, 163, 0.5);
+    }
+  }
+}
+</style>
