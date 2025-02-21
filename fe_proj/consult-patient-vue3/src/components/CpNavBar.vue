5875df4e055326a58df91807f998fd9350d53ed0
vencent2006 <lewis_sun@foxmail.com>
2025-02-21 19:03:33 +0800

    feat: day5-3 cp-nav-bar的结构和样式

---
+++ fe_proj/consult-patient-vue3/src/components/CpNavBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+<template>
+  <van-nav-bar fixed left-arrow title="登录" right-text="注册"></van-nav-bar>
+</template>
+<style lang="scss" scoped>
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- fe_proj/consult-patient-vue3/src/views/Login/index.vue
+++ fe_proj/consult-patient-vue3/src/views/Login/index.vue
@@ -3,9 +3,11 @@ import {} from 'vue'
 </script>
 
 <template>
-  <div class="login-page">login</div>
-  <van-button>van-button</van-button>
-  <van-checkbox>van-checkbox</van-checkbox>
+  <div class="login-page">
+    <cp-nav-bar></cp-nav-bar>
+    <van-button>van-button</van-button>
+    <van-checkbox>van-checkbox</van-checkbox>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
