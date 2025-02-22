4928c6bc77fb1898d4466a49d10bff9ed983ebff
vencent2006 <lewis_sun@foxmail.com>
2025-02-22 20:17:45 +0800

    feat: day5-8 登录模块表单校验

---
+++ fe_proj/consult-patient-vue3/src/utils/rules.ts
@@ -0,0 +1,16 @@
+// 提供校验规则
+
+import type { FieldRule } from 'vant'
+
+// 校验手机号
+const mobileRules: FieldRule[] = [
+  { required: true, message: '请输入手机号' },
+  { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确' },
+]
+
+const passwordRules: FieldRule[] = [
+  { required: true, message: '请输入密码' },
+  { pattern: /^\w{8,24}$/, message: '密码必须是8-24位' },
+]
+
+export { mobileRules, passwordRules }
--- fe_proj/consult-patient-vue3/src/views/Login/index.vue
+++ fe_proj/consult-patient-vue3/src/views/Login/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-const fn = () => {
-  console.log('click right')
+import { ref } from 'vue'
+import { mobileRules, passwordRules } from '@/utils/rules'
+import { showToast } from 'vant'
+const mobile = ref('')
+const password = ref('')
+const agree = ref(false)
+const onSubmit = () => {
+  if (!agree.value) return showToast('请勾选协议')
+  // TODO 登录
 }
 </script>
 
@@ -16,12 +23,22 @@ const fn = () => {
       </a>
     </div>
     <!-- 表单 -->
-    <van-form autocomplete="off">
-      <van-field placeholder="请输入手机号" type="tel"></van-field>
-      <van-field placeholder="请输入密码" type="password"></van-field>
+    <van-form autocomplete="off" @submit="onSubmit">
+      <van-field
+        v-model="mobile"
+        :rules="mobileRules"
+        placeholder="请输入手机号"
+        type="tel"
+      ></van-field>
+      <van-field
+        v-model="password"
+        :rules="passwordRules"
+        placeholder="请输入密码"
+        type="password"
+      ></van-field>
 
       <div class="cp-cell">
-        <van-checkbox>
+        <van-checkbox v-model="agree">
           <span>我已同意</span>
           <a href="javascript:;">用户协议</a>
           <span>及</span>
