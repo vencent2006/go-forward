1a5d2983f59df8104e99bed3bc9402faac1c8ce7
vencent2006 <lewis_sun@foxmail.com>
2025-02-24 15:58:03 +0800

    feat: day5-14 svg组件的封装

--- fe_proj/consult-patient-vue3/src/types/component.d.ts
+++ fe_proj/consult-patient-vue3/src/types/component.d.ts
@@ -1,8 +1,10 @@
-import type CpNavBar from '@/components/CpNavBar.vue'
+import CpNavBar from '@/components/CpNavBar.vue'
+import CpIcon from '@/components/CpIcon.vue'
 
 declare module 'vue' {
   interface GlobalComponents {
     // 添加全局组件类型
     CpNavBar: typeof CpNavBar // typeof 作用：从js变量中取出对应的类型
+    CpIcon: typeof CpIcon // svg图标
   }
 }
