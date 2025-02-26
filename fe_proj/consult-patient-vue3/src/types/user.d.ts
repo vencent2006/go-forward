be9a97f85a7b622b02fcda4af24d9448fa99deec
vencent2006 <lewis_sun@foxmail.com>
2025-02-26 17:31:36 +0800

    feat: day6-6 个人中心-用户信息类型

--- fe_proj/consult-patient-vue3/src/types/user.d.ts
+++ fe_proj/consult-patient-vue3/src/types/user.d.ts
@@ -12,3 +12,19 @@ export type User = {
 }
 
 export type CodeType = 'login' | 'register' | 'changeMobile' | 'forgetPassword' | 'bindMobile'
+
+// Omit 排除 | Pick 选择(摘取)
+type OmitUser = Omit<User, 'token'> // 排除token的类型
+
+export type UserInfo = OmitUser & {
+  likeNumber: number // 关注数量
+  collectionNumber: number // 收藏数量
+  score: number // 积分
+  couponNumber: number // 优惠券数量
+  orderInfo: {
+    paidNumber: number // 待付款
+    receivedNumber: number // 待收货
+    shippedNumber: number // 待发货
+    finishedNumber: number // 已完成
+  }
+}
