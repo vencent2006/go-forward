43a06a8952fc649915c969116a94e982a484c098
vencent2006 <lewis_sun@foxmail.com>
2025-03-04 15:12:20 +0800

    feat: day7-12 极速问诊-问诊信息类型定义

---
+++ fe_proj/consult-patient-vue3/src/enums/index.ts
@@ -0,0 +1,14 @@
+// 问诊类型
+export enum ConsultType {
+  Doctor = 1, // 找医生
+  Fast = 2, // 极速问诊
+  Medication = 3, // 开药问诊
+}
+
+// 问诊时间
+export enum IllnessTime {
+  Week = 1,
+  Month = 2,
+  HalfYear = 3,
+  More = 4, // 超过半年
+}
