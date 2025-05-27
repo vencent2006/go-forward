0e719419b7639f2c4a516734b0f1165bd4a56428
vencent2006 <lewis_sun@foxmail.com>
2025-05-27 15:30:29 +0800

    wali bubble sort demo

--- algorithm/lucas/wali/1234-2.cpp
+++ algorithm/lucas/wali/1234-2.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-int a[100];
+int a[10];
 
 int main() {
     int n;
     cin >> n;
+    int sum = 0;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
+        sum += a[i];
     }
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (a[j] > a[j + 1]) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] < a[j + 1]) {
                 swap(a[j], a[j + 1]);
             }
         }
@@ -18,5 +20,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
     }
+    cout << endl << sum;
+
     return 0;
 }
